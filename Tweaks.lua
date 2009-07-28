f796cf6cf4f8ab40005c05fe0271bbc6ce972abd
Adrian L Lange <adrianlund@gmail.com>
2009-07-28 20:07:39 +0200

    Added mail receipt

--- Tweaks.lua
+++ Tweaks.lua
@@ -21,3 +21,20 @@ Inomena:Register('UPDATE_PENDING_MAIL', function()
 		PlaySoundFile([=[Interface\AddOns\Inomena\mail.wav]=])
 	end
 end)
+
+--[[ Last receipt in mail ]]
+do
+	local last
+	Inomena:Register('MAIL_SEND_SUCCESS', function()
+		if(last) then
+			SendMailNameEditBox:SetText(last)
+			SendMailNameEditBox:HighlightText()
+		end
+	end
+
+	local orig = SendMailFrame_SendMail
+	function SendMailFrame_SendMail(...)
+		last = SendMailFrameEditBox:GetText()
+		orig(...)
+	end
+end
