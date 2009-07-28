6795e88d72cda97d84ae78e087e0f7a71b5b55d1
Adrian L Lange <adrianlund@gmail.com>
2009-07-28 19:59:36 +0200

    Added copy/paste function

---
+++ Tweaks.lua
@@ -0,0 +1,16 @@
+--[[ Copy/paste function to colorpicker ]]
+Inomena:Register('PLAYER_LOGIN', function(r, g, b)
+	local copy = CreateFrame('Button', nil, ColorPickerFrame, 'UIPanelButtonTemplate')
+	copy:SetPoint('BOTTOMLEFT', ColorPickerFrame, 'TOPLEFT', 208, -110)
+	copy:SetHeight(20)
+	copy:SetWidth(80)
+	copy:SetText('Copy')
+	copy:SetScript('OnClick', function() r, g, b = ColorPickerFrame:GetColorRGB() end)
+
+	local paste = CreateFrame('Button', nil, ColorPickerFrame, 'UIPanelButtonTemplate')
+	paste:SetPoint('BOTTOMLEFT', ColorPickerFrame, 'TOPLEFT', 208, -135)
+	paste:SetHeight(20)
+	paste:SetWidth(80)
+	paste:SetText('Paste')
+	paste:SetScript('OnClick', function() ColorPickerFrame:SetColorRGB(r, g, b) end)
+end)
