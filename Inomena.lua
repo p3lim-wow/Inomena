95bdb8c0c72e748ca5b7ba06de4b6e47b3c696d2
Adrian L Lange <adrianlund@gmail.com>
2009-08-03 00:50:14 +0200

    Added print function

--- Inomena.lua
+++ Inomena.lua
@@ -5,4 +5,8 @@ function Inomena:Register(event, method)
 	self:RegisterEvent(event)
 end
 
+function Inomena:Print(...)
+	print('|cffff8080Inomena:|r', ...)
+end
+
 Inomena:SetScript('OnEvent', function(self, event, ...) self[event](self, event, ...) end)
