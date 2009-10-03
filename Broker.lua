d4b4db107c1276d0b369cce7ab23ebba994e903c
Adrian L Lange <adrianlund@gmail.com>
2009-10-03 18:19:21 +0200

    Remove Broker_SimpleDPS and fix Broker_Equipement support

--- Broker.lua
+++ Broker.lua
@@ -1,33 +1,6 @@
 local gear = cargoShip('Broker_Equipment', {noText = true})
 gear:SetPoint('BOTTOMRIGHT', Minimap, -1, 1)
-gear:SetScript('OnLeave', function() gear:SetAlpha(0) end)
-gear:SetScript('OnEnter', function() gear:SetAlpha(1) end)
+gear:HookScript('OnLeave', function() gear:SetAlpha(0) end)
+gear:HookScript('OnEnter', function() gear:SetAlpha(1) end)
+gear:SetWidth(gear:GetHeight())
 gear:SetAlpha(0)
-
-
-local dps = cargoShip('Broker_SimpleDPS', {
-	noShadow = true,
-	noIcon = true,
-	font = [=[Interface\AddOns\Inomena\media\marke.ttf]=],
-	fontStyle = 'OUTLINE',
-	fontSize = 9,
-})
-dps:SetPoint('BOTTOMLEFT', Minimap)
-dps:SetScript('OnEnter', function() dps:SetAlpha(1) end)
-dps:SetScript('OnLeave', function()
-	if(not UnitAffectingCombat('player')) then
-		dps:SetAlpha(0)
-	end
-end)
-
-local function fadeText()
-	if(UnitAffectingCombat('player')) then
-		dps:SetAlpha(1)
-	else
-		dps:SetAlpha(0)
-	end
-end
-
-Inomena:Register('PLAYER_REGEN_ENABLED', fadeText)
-Inomena:Register('PLAYER_REGEN_DISABLED', fadeText)
-fadeText()
