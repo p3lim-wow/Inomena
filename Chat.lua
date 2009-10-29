2930f9d38a415d14d325473610fa8ed871df8da0
Adrian L Lange <adrianlund@gmail.com>
2009-10-29 23:18:30 +0100

    Clean up and simplify the chat module

--- Chat.lua
+++ Chat.lua
@@ -1,57 +1,43 @@
-local orig = {}
+
 local gsub = string.gsub
+local format = string.format
 
-local function poof(obj)
-	obj.Show = obj.Hide
-	obj:Hide()
+local hooks = {}
+local strings = {
+	[CHAT_MSG_GUILD] = 'g',
+	[CHAT_MSG_PARTY] = 'p',
+	[CHAT_MSG_RAID] = 'r',
+	[CHAT_MSG_RAID_WARNING] = 'w',
+	[CHAT_MSG_RAID_LEADER] = '|cffffff00r|r',
+	[CHAT_MSG_BATTLEGROUND] = 'b',
+	[CHAT_MSG_BATTLEGROUND_LEADER] = '|cffffff00b|r',
+}
+
+local function ReplaceStrings(channel, orig)
+	return format('|Hchannel:%s|h%s|h', channel, strings[orig] or channel)
 end
 
-local function tellTarget(str)
-	if(UnitIsPlayer('target') and UnitIsFriend('player', 'target')) then
-		SendChatMessage(str, 'WHISPER', GetDefaultLanguage('player'), GetUnitName('target', 'true'):gsub('%s', '', 2))
-	end
-end
+local function AddMessage(self, str, ...)
+	if(not str) then return hooks[self](self, str, ...) end
 
-local function addMessage(self, str, ...)
-	str = str:gsub('^|Hchannel:(%d+)|h(%[([%d%. ]*)([^%]]+)%])|h', '|Hchannel:%1|h%1|h')
 	str = str:gsub('(|Hplayer.-|h)%[(.-)%]|h', '%1%2|h')
+	str = str:gsub('^|Hchannel:(.-)|h%[(.-)%]|h', ReplaceStrings)
 
-	str = str:gsub('^To (.-|h):', '%1:|cffcc6699')
-	str = str:gsub('^(.-|h) whispers:', '%1:')
-	str = str:gsub('^(.-|h) says:', '%1:')
-	str = str:gsub('^(.-|h) yells:', '%1:')
+	str = str:gsub('(|Hplayer.-|h) has earned the achievement (.-)!', '%1 ! %2')
+	str = str:gsub('^To (.-|h)', 't %1')
+	str = str:gsub('^(.-|h) whispers', '%1')
+	str = str:gsub('^(.-|h) says', '%1')
+	str = str:gsub('^(.-|h) yells', '%1')
 
-	str = str:gsub('^(%[('..RAID_WARNING..')%])', 'w')
-
-	str = str:gsub('^|Hchannel:o|h(%[(.-)%])|h', '|Hchannel:o|ho|h')
-	str = str:gsub('^|Hchannel:Guild|h(%[(.-)%])|h', '|Hchannel:Guild|hg|h')
-	str = str:gsub('^|Hchannel:Party|h(%[(.-)%])|h', '|Hchannel:Party|hp|h')
-	str = str:gsub('^|Hchannel:Battleground|h(%[('..CHAT_MSG_BATTLEGROUND_LEADER..')%])|h', '|Hchannel:Battleground|h|cffffff00b|r|h')
-	str = str:gsub('^|Hchannel:Battleground|h(%[('..CHAT_MSG_BATTLEGROUND..')%])|h', '|Hchannel:Battleground|hb|h')
-	str = str:gsub('^|Hchannel:raid|h(%[('..CHAT_MSG_RAID_LEADER..')%])|h', '|Hchannel:raid|h|cffffff00r|r|h')
-	str = str:gsub('^|Hchannel:raid|h(%[('..CHAT_MSG_RAID..')%])|h', '|Hchannel:raid|hr|h')	
-
-	return orig[self](self, str, ...)
+	return hooks[self](self, str, ...)
 end
 
-local function onSpacePressed(self)
-	local text = string.lower(self:GetText())
-	if(string.sub(text, 1, 1) == '/' and string.match(text, '^/tt $')) then
-		if(UnitIsPlayer('target') and UnitIsFriend('player', 'target')) then
-			self:Hide()
-			self:SetAttribute('chatType', 'WHISPER')
-			self:SetAttribute('tellTarget', GetUnitName('target', 'true'):gsub('%s', '', 2))
-			ChatFrame_OpenChat('')
-		end
-	end
-end
-
-local function onMouseWheel(self, dir)
+local function OnMouseWheel(self, dir)
 	if(dir > 0) then
 		if(IsShiftKeyDown()) then
 			self:ScrollToTop()
 		elseif(IsControlKeyDown()) then
-			self:ScrollUp(); self:ScrollUp(); self:ScrollUp()
+			self:PageUp()
 		else
 			self:ScrollUp()
 		end
@@ -59,43 +45,62 @@ local function onMouseWheel(self, dir)
 		if(IsShiftKeyDown()) then
 			self:ScrollToBottom()
 		elseif(IsControlKeyDown()) then
-			self:ScrollDown(); self:ScrollDown(); self:ScrollDown()
+			self:PageDown()
 		else
 			self:ScrollDown()
 		end
 	end
 end
 
-for i = 1, NUM_CHAT_WINDOWS do
-	local frame = _G['ChatFrame'..i]
-	frame:EnableMouseWheel()
-	frame:SetScript('OnMouseWheel', onMouseWheel)
-	frame:SetFont([=[Interface\AddOns\Inomena\media\vera.ttf]=], 12)
-
-	poof(_G['ChatFrame'..i..'UpButton'])
-	poof(_G['ChatFrame'..i..'DownButton'])
-	poof(_G['ChatFrame'..i..'BottomButton'])
-
-	orig[frame] = frame.AddMessage
-	frame.AddMessage = addMessage
+local function Poof(self)
+	self.Show = self.Hide
+	self:Hide()
 end
 
-poof(ChatFrameMenuButton)
+for index = 1, NUM_CHAT_WINDOWS do
+	local frame = _G['ChatFrame'..index]
+	frame:EnableMouseWheel()
+	frame:SetScript('OnMouseWheel', OnMouseWheel)
+	frame:SetFont([=[Interface\AddOns\Inomena\media\vera.ttf]=], 12)
 
-DEFAULT_CHATFRAME_ALPHA = 0
-CHAT_TELL_ALERT_TIME = 0
-ChatTypeInfo.OFFICER.sticky = 1
+	Poof(_G['ChatFrame'..index..'UpButton'])
+	Poof(_G['ChatFrame'..index..'DownButton'])
+	Poof(_G['ChatFrame'..index..'BottomButton'])
 
-local regions = {ChatFrameEditBox:GetRegions()}
-poof(regions[6]); poof(regions[7]); poof(regions[8])
+	hooks[frame] = frame.AddMessage
+	frame.AddMessage = AddMessage
+end
 
-ChatFrameEditBox:ClearAllPoints()
-ChatFrameEditBox:SetPoint('BOTTOMLEFT', ChatFrame1, 'TOPLEFT', -5, 20)
-ChatFrameEditBox:SetPoint('BOTTOMRIGHT', ChatFrame1, 'TOPRIGHT', -5, 20)
-ChatFrameEditBox:SetAltArrowKeyMode(nil)
-ChatFrameEditBox:SetFont([=[Interface\AddOns\Inomena\media\vera.ttf]=], 14)
+do
+	local editbox = ChatFrameEditBox
+	editbox:ClearAllPoints()
+	editbox:SetPoint('BOTTOMLEFT', ChatFrame1, 'TOPLEFT', -5, 20)
+	editbox:SetPoint('BOTTOMRIGHT', ChatFrame1, 'TOPRIGHT', 5, 20)
+	editbox:SetFont([=[Interface\AddOns\Inomena\media\vera.ttf]=], 14)
+	editbox:SetAltArrowKeyMode(false)
+
+	local regions = {editbox:GetRegions()}
+	regions[6]:Hide()
+	regions[7]:Hide()
+	regions[8]:Hide()
+
+	ChatFrameMenuButton:Hide()
+	DEFAULT_CHATFRAME_ALPHA = 0
+	CHAT_TELL_ALERT_TIME = 0
+end
 
 SLASH_TellTarget1 = '/tt'
-SlashCmdList.TellTarget = tellTarget
+SlashCmdList.TellTarget = function(str)
+	if(UnitIsPlayer('target') and UnitIsFriend('player', 'target')) then
+		SendChatMessage(str, 'WHISPER', GetDefaultLanguage('player'), GetUnitName('target', true):gsub('%s', '', 2))
+	end
+end
 
-hooksecurefunc('ChatEdit_OnSpacePressed', onSpacePressed)
+hooksecurefunc('ChatEdit_OnSpacePressed', function(self)
+	if(string.match(string.lower(self:GetText()), '^/tt $') and UnitIsPlayer('target') and UnitIsFriend('player', 'target')) then
+		self:Hide()
+		self:SetAttribute('chatType', 'WHISPER')
+		self:SetAttribute('tellTarget', GetUnitName('target', true):gsub('%s', '', 2))
+		ChatFrame_OpenChat('')
+	end
+end)
