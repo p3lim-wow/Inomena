d48aad053ff445883bd009da66ef27d4c4a0c842
Adrian L Lange <adrianlund@gmail.com>
2010-02-28 00:42:03 +0100

    Replace LibCargoShip with own code

--- Broker.lua
+++ Broker.lua
@@ -1,6 +1,22 @@
-local gear = LibStub('LibCargoShip-2.1'):CreateBlock('Broker_Equipment', {noText = true})
-gear:SetPoint('BOTTOMRIGHT', Minimap, -1, 1)
-gear:HookScript('OnLeave', function() gear:SetAlpha(0) end)
-gear:HookScript('OnEnter', function() gear:SetAlpha(1) end)
-gear:SetWidth(gear:GetHeight())
-gear:SetAlpha(0)
+local LDB = LibStub('LibDataBroker-1.1')
+
+local data = LDB:GetDataObjectByName('Broker_Equipment')
+if(data) then
+	local object = CreateFrame('Button', nil, UIParent)
+	object:SetPoint('BOTTOMRIGHT', Minimap, -1, 1)
+	object:RegisterForClicks('AnyUp')
+	object:SetHeight(12)
+	object:SetWidth(12)
+	object:SetAlpha(0)
+	object:SetScript('OnLeave', function(self) self:SetAlpha(0) end)
+	object:SetScript('OnEnter', function(self) self:SetAlpha(1) end)
+	object:SetScript('OnClick', data.OnClick)
+
+	local icon = object:CreateTexture(nil, 'OVERLAY')
+	icon:SetAllPoints(object)
+	icon:SetTexture(data.icon)
+
+	LDB.RegisterCallback(object, 'LibDataBroker_AttributeChanged_Broker_Equipment', function()
+		icon:SetTexture(data.icon)
+	end)
+end
