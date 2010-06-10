cc3fe052dd7962c92169fa91c798c55abbada90f
Adrian L Lange <adrianlund@gmail.com>
2010-06-10 20:11:25 +0200

    Remove color copy/paste buttons

--- Tweaks.lua
+++ Tweaks.lua
@@ -1,22 +1,5 @@
 local _, ns = ...
 
---[[ Copy/paste ]]
-ns.Register('PLAYER_LOGIN', function(r, g, b)
-	local copy = CreateFrame('Button', nil, ColorPickerFrame, 'UIPanelButtonTemplate')
-	copy:SetPoint('BOTTOMLEFT', ColorPickerFrame, 'TOPLEFT', 208, -110)
-	copy:SetHeight(20)
-	copy:SetWidth(80)
-	copy:SetText('Copy')
-	copy:SetScript('OnClick', function() r, g, b = ColorPickerFrame:GetColorRGB() end)
-
-	local paste = CreateFrame('Button', nil, ColorPickerFrame, 'UIPanelButtonTemplate')
-	paste:SetPoint('BOTTOMLEFT', ColorPickerFrame, 'TOPLEFT', 208, -135)
-	paste:SetHeight(20)
-	paste:SetWidth(80)
-	paste:SetText('Paste')
-	paste:SetScript('OnClick', function() ColorPickerFrame:SetColorRGB(r, g, b) end)
-end)
-
 --[[ Last receipt in mail ]]
 do
 	local last
