57f2826a5a7e8b3a55fb416728219abff49c7c71
Adrian L Lange <adrianlund@gmail.com>
2010-06-10 20:08:26 +0200

    Improve :Register() - Taken from haste's Kousei

--- Inomena.lua
+++ Inomena.lua
@@ -1,9 +1,30 @@
-local _, ns = ...
+local _, Inomena = ...
 
 local addon = CreateFrame('Frame')
-addon:SetScript('OnEvent', function(self, event, ...) self[event](self, event, ...) end)
+addon:SetScript('OnEvent', function(self, event, ...) self[event](...) end)
 
-ns.Register = function(event, method)
-	addon[event] = method
-	addon:RegisterEvent(event)
+local metatable = {
+	__call = function(funcs, self, ...)
+		for _, func in pairs(funcs) do
+			func(self, ...)
+		end
+	end
+}
+
+Inomena.Register = function(event, method)
+	local current = addon[event]
+	if(current and method) then
+		if(type(current) == 'function') then
+			addon[event] = setmetatable({current, method}, metatable)
+		else
+			for _, func in pairs(current) do
+				if(func == method) then return end
+			end
+
+			table.insert(current, method)
+		end
+	else
+		addon[event] = method
+		addon:RegisterEvent(event)
+	end
 end
