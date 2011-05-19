20384f7a0194c79a161c6247f48122a04aecef70
Adrian L Lange <adrianlund@gmail.com>
2011-05-20 00:21:57 +0200

    Remove boss frames

--- diaf.lua
+++ diaf.lua
@@ -1,4 +1,10 @@
 
+for index = 1, MAX_BOSS_FRAMES do
+	local frame = _G['Boss' .. index .. 'TargetFrame']
+	frame:UnregisterAllEvents()
+	frame:Hide()
+end
+
 UIErrorsFrame:UnregisterEvent('UI_ERROR_MESSAGE')
 
 function CombatLog_LoadUI() end
