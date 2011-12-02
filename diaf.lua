7e3a33343577577439989a5bbf46cb39cb401730
Adrian L Lange <adrianlund@gmail.com>
2011-12-02 20:59:03 +0100

    Remove bugged button

--- diaf.lua
+++ diaf.lua
@@ -17,6 +17,9 @@ VehicleSeatIndicator:Hide()
 
 UIErrorsFrame:UnregisterEvent('UI_ERROR_MESSAGE')
 
+-- Stupid leftover by blizz, bugs out the new window a bit
+FriendsFrameTab4:Hide()
+
 function CombatLog_LoadUI() end
 
 -- Fuck you Blizzard
