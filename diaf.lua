cd3812b50b97bf907aa0fae19a8960dadb7ac88a
Adrian L Lange <adrianlund@gmail.com>
2012-04-28 19:42:20 +0200

    LFR crap keeps tainting everything

--- diaf.lua
+++ diaf.lua
@@ -7,3 +7,6 @@ UIErrorsFrame:UnregisterEvent('UI_ERROR_MESSAGE')
 
 -- Stupid leftover by blizz, bugs out the new window a bit
 FriendsFrameTab4:Hide()
+
+-- Fuck your taints
+LFRParentFrame:SetScript('OnHide', nil)
