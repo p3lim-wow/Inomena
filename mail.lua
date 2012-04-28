0006316db6dcd35811df9d9232272265c79e05ad
Adrian L Lange <adrianlund@gmail.com>
2012-04-28 19:35:56 +0200

    Add a get-all-mail button

--- mail.lua
+++ mail.lua
@@ -32,3 +32,76 @@ Inomena.RegisterEvent('UI_ERROR_MESSAGE', function(msg)
 		SendMailMailButton:Click()
 	end
 end)
+
+do
+	local button = CreateFrame('Button', nil, InboxFrame, 'UIPanelButtonTemplate')
+
+	local function GetFreeSlots()
+		local slots = 0
+		for bag = BACKPACK_CONTAINER, NUM_BAG_SLOTS do
+			local free, family = GetContainerNumFreeSlots(bag)
+			if(family == 0) then
+				slots = slots + free
+			end
+		end
+
+		return slots
+	end
+
+	local skipNum, lastNum
+	local function GetMail()
+		if(GetInboxNumItems() - skipNum <= 0) then
+			button:Enable()
+			button:UnregisterEvent('MAIL_INBOX_UPDATE')
+			return
+		end
+
+		local index = 1 + skipNum
+		local _, _, _, _, money, cod, _, multiple, _, _, _, _, _, single = GetInboxHeaderInfo(index)
+		if(cod > 0) then
+			skipNum = skipNum + 1
+			GetMail(self)
+		elseif(money > 0) then
+			TakeInboxMoney(index)
+		elseif(single and (GetFreeSlots() > 6)) then
+			AutoLootMailItem(index)
+		elseif(multiple and (GetFreeSlots() + multiple > 6)) then
+			AutoLootMailItem(index)
+		end
+	end
+
+	button:SetScript('OnEvent', function(self)
+		local num = GetInboxNumItems()
+		if(lastNum ~= num) then
+			lastNum = num
+		else
+			return
+		end
+
+		GetMail(self)
+	end)
+
+	button:SetScript('OnClick', function(self)
+		self:RegisterEvent('MAIL_INBOX_UPDATE')
+		self:Disable()
+
+		lastNum = 0
+		skipNum = 0
+
+		GetMail(self)
+	end)
+
+	local initialized
+	Inomena.RegisterEvent('MAIL_SHOW', function()
+		if(initialized) then
+			button:Enable()
+			return
+		end
+
+		button:SetPoint('BOTTOM', -12, 88)
+		button:SetSize(90, 25)
+		button:SetText(OPENMAIL)
+
+		initialized = true
+	end)
+end
