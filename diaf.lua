c350e7fdda25b2efa64c742be9ac0681548895cc
Adrian L Lange <adrianlund@gmail.com>
2012-08-23 22:36:00 +0200

    They actually fixed it

--- diaf.lua
+++ diaf.lua
@@ -4,9 +4,3 @@ VehicleSeatIndicator:UnregisterAllEvents()
 VehicleSeatIndicator:Hide()
 
 UIErrorsFrame:UnregisterEvent('UI_ERROR_MESSAGE')
-
--- Stupid leftover by blizz, bugs out the new window a bit
-FriendsFrameTab4:Hide()
-
--- Fuck your taints
-LFRParentFrame:SetScript('OnHide', nil)
