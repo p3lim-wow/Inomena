546372a79e405678b0a05d89593038a13805e9b0
Adrian L Lange <adrianlund@gmail.com>
2013-06-02 01:17:16 +0200

    More notifications, MORE!

--- notify.lua
+++ notify.lua
@@ -1,19 +1,27 @@
 local __, Inomena = ...
 
+local soundFile = [=[Sound\Interface\ReadyCheck.wav]=]
+
 Inomena.RegisterEvent('UPDATE_BATTLEFIELD_STATUS', function()
 	if(StaticPopup_Visible('CONFIRM_BATTLEFIELD_ENTRY')) then
-		PlaySoundFile([=[Sound\Interface\ReadyCheck.wav]=], 'Master')
+		PlaySoundFile(soundFile, 'Master')
 	end
 end)
 
 Inomena.RegisterEvent('LFG_PROPOSAL_SHOW', function()
-	PlaySoundFile([=[Sound\Interface\ReadyCheck.wav]=], 'Master')
+	PlaySoundFile(soundFile, 'Master')
 end)
 
 ReadyCheckListenerFrame:SetScript('OnShow', function()
-	PlaySoundFile([=[Sound\Interface\ReadyCheck.wav]=], 'Master')
+	PlaySoundFile(soundFile, 'Master')
 end)
 
-Inomena.RegisterEvent('PARTY_INVITE_REQUEST', function(name)
-	PlaySoundFile([=[Sound\Interface\ReadyCheck.wav]=], 'Master')
+Inomena.RegisterEvent('PARTY_INVITE_REQUEST', function()
+	PlaySoundFile(soundFile, 'Master')
+end)
+
+Inomena.RegisterEvent('CHAT_MSG_RAID_BOSS_WHISPER', function(msg, name)
+	if(name == UnitName('player') and msg == 'You are next in line!') then
+		PlaySoundFile(soundFile, 'Master')
+	end
 end)
