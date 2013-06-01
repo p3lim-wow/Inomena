f92bfaddb98c7ddd4e7bf8285ca9acd482205cd5
Adrian L Lange <adrianlund@gmail.com>
2013-06-02 01:17:06 +0200

    More stupid blizz bugs

--- diaf.lua
+++ diaf.lua
@@ -4,3 +4,9 @@ VehicleSeatIndicator:UnregisterAllEvents()
 VehicleSeatIndicator:Hide()
 
 UIErrorsFrame:UnregisterEvent('UI_ERROR_MESSAGE')
+
+hooksecurefunc('StaticPopup_Show', function(which)
+	if(which == 'DEATH' and not UnitIsDead('player')) then
+		StaticPopup_Hide(which)
+	end
+end)
