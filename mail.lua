621b67c01c9c6888546daaff718ff9718e742fcb
Adrian L Lange <adrianlund@gmail.com>
2014-07-21 03:31:23 +0200

    Move the mail tracking to p3lim-wow/Relief

--- mail.lua
+++ mail.lua
@@ -14,19 +14,6 @@ do
 	end)
 end
 
-Inomena.RegisterEvent('UPDATE_PENDING_MAIL', function()
-	for index = 1, GetNumTrackingTypes() do
-		local name, texture, active = GetTrackingInfo(index)
-		if(name == MINIMAP_TRACKING_MAILBOX) then
-			if(HasNewMail() and not active) then
-				return SetTracking(index, true)
-			elseif(not HasNewMail() and active) then
-				return SetTracking(index, false)
-			end
-		end
-	end
-end)
-
 Inomena.RegisterEvent('UI_ERROR_MESSAGE', function(msg)
 	if(msg == ERR_MAIL_INVALID_ATTACHMENT_SLOT) then
 		SendMailMailButton:Click()
