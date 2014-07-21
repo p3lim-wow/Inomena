12a68aade2911f83c41a9aedb7baa4c0bc13ffe0
Adrian L Lange <adrianlund@gmail.com>
2014-07-21 03:30:57 +0200

    Remove a fix that got fixed

--- diaf.lua
+++ diaf.lua
@@ -4,9 +4,3 @@ VehicleSeatIndicator:UnregisterAllEvents()
 VehicleSeatIndicator:Hide()
 
 UIErrorsFrame:UnregisterEvent('UI_ERROR_MESSAGE')
-
-hooksecurefunc('StaticPopup_Show', function(which)
-	if(which == 'DEATH' and not UnitIsDead('player')) then
-		StaticPopup_Hide(which)
-	end
-end)
