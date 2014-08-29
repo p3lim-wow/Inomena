5de27de1cd4ea27ecde314b49984ee5a521d8acf
Adrian L Lange <adrianlund@gmail.com>
2014-08-29 16:32:40 +0200

    Add coordinates to the world map

--- tweaks.lua
+++ tweaks.lua
@@ -98,6 +98,33 @@ Inomena.RegisterEvent('CINEMATIC_STOP', function()
 end)
 
 if(Inomena.WoD) then
+	local CoordText
+	local totalElapsed = 0
+	local function UpdateCoords(self, elapsed)
+		if(totalElapsed > 0.1) then
+			if(self:IsMouseOver()) then
+				local scale = self:GetEffectiveScale()
+				local centerX, centerY = self:GetCenter()
+				local width, height = self:GetSize()
+				local x, y = GetCursorPosition()
+
+				x = ((x / scale) - (centerX - (width / 2))) / width
+				y = (centerY + (height / 2) - (y / scale)) / height
+
+				CoordText:SetFormattedText('%.2f, %.2f', x * 100, y * 100)
+				CoordText:SetTextColor(0, 1, 0)
+			else
+				local x, y = GetPlayerMapPosition('player')
+				CoordText:SetFormattedText('%.2f, %.2f', x * 100, y * 100)
+				CoordText:SetTextColor(1, 1, 0)
+			end
+
+			totalElapsed = 0
+		else
+			totalElapsed = totalElapsed + elapsed
+		end
+	end
+
 	Inomena.RegisterEvent('PLAYER_LOGIN', function()
 		ObjectiveTrackerFrame:ClearAllPoints()
 		ObjectiveTrackerFrame:SetPoint('TOPLEFT', 50, -142)
@@ -105,6 +132,11 @@ if(Inomena.WoD) then
 
 		ObjectiveTrackerFrame.ClearAllPoints = Inomena.null
 		ObjectiveTrackerFrame.SetPoint = Inomena.null
+
+		CoordText = WorldMapFrameCloseButton:CreateFontString(nil, nil, 'GameFontNormal')
+		CoordText:SetPoint('RIGHT', WorldMapFrameCloseButton, 'LEFT', -30, 0)
+
+		WorldMapDetailFrame:HookScript('OnUpdate', UpdateCoords)
 	end)
 end
 
