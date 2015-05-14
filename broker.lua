8f056f3cf1cedc369e843633d3c88ed39d82dd1d
Adrian L Lange <mail@p3lim.net>
2015-05-14 15:57:23 +0200

    Add LDB icon for BugSack on the Minimap
    Moved from p3lim-wow/Relief

---
+++ broker.lua
@@ -0,0 +1,40 @@
+local _, Inomena = ...
+
+Inomena.RegisterEvent('PLAYER_LOGIN', function()
+	local LDB = LibStub('LibDataBroker-1.1')
+	if(not LDB --[[or not IsAddOnLoaded('BugSack')]]) then
+		return
+	end
+
+	local data = LDB:GetDataObjectByName('BugSack')
+	if(data) then
+		local Button = CreateFrame('Button', nil, Minimap)
+		Button:SetPoint('BOTTOMRIGHT', -5, 5)
+		Button:SetSize(20, 20)
+		Button:SetScript('OnClick', data.OnClick)
+		Button:SetScript('OnLeave', GameTooltip_Hide)
+		Button:SetScript('OnEnter', function(self)
+			GameTooltip:SetOwner(self, 'ANCHOR_LEFT')
+			GameTooltip:SetClampedToScreen(true)
+			pcall(data.OnTooltipShow, GameTooltip)
+			GameTooltip:Show()
+		end)
+
+		if(not string.find(data.icon, 'red')) then
+			Button:SetAlpha(0)
+		end
+
+		local Icon = Button:CreateTexture(nil, 'OVERLAY')
+		Icon:SetTexture([[Interface\CharacterFrame\UI-Player-PlayTimeUnhealthy]])
+		Icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+		Icon:SetAllPoints()
+
+		LDB.RegisterCallback(Button, 'LibDataBroker_AttributeChanged_BugSack', function()
+			if(string.find(data.icon, 'red')) then
+				Button:SetAlpha(1)
+			else
+				Button:SetAlpha(0)
+			end
+		end)
+	end
+end)
