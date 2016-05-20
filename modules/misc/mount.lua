04454534589e8226875e73ed3fcc2dcf7c5d329b
Adrian L Lange <mail@p3lim.net>
2016-05-20 19:29:26 +0200

    misc/mount: Update for API changes in Legion

--- modules/misc/mount.lua
+++ modules/misc/mount.lua
@@ -1,4 +1,4 @@
-local E, F = unpack(select(2, ...))
+local E, F, C = unpack(select(2, ...))
 
 local buttonName = (...) .. 'MountButton'
 local bindingString = string.format('CLICK %s:LeftButton', buttonName)
@@ -14,7 +14,7 @@ local Button = CreateFrame('Button', buttonName, nil, 'SecureActionButtonTemplat
 Button:SetAttribute('type', 'macro')
 
 local function HasCorralOutpost()
-	if(HasDraenorZoneAbility() and not IsFlyableArea()) then
+	if((C.isBetaClient and HasZoneAbility or HasDraenorZoneAbility)() and not IsFlyableArea()) then
 		local ability = GetSpellInfo(161691)
 		local _, _, _, _, _, _, spellID = GetSpellInfo(ability)
 		if(spellID == 164222 or spellID == 165803) then
@@ -43,7 +43,11 @@ local function Update()
 		if(spellName) then
 			macro = '/cast ' .. spellName
 		else
-			macro = '/run C_MountJournal.Summon(0)'
+			if(C.isBetaClient) then
+				macro = '/run C_MountJournal.SummonByID(0)'
+			else
+				macro = '/run C_MountJournal.Summon(0)'
+			end
 		end
 	end
 
@@ -56,11 +60,22 @@ end
 
 local function UpdateFavorites(event, newLevel)
 	local taughtRiding = IsSpellKnown(33388)
-	for index = 1, C_MountJournal.GetNumMounts() do
-		local _, spellID, _, _, _, _, _, _, _, _, collected = C_MountJournal.GetMountInfo(index)
-		if(spellID == 179244 and collected) then
-			C_MountJournal.SetIsFavorite(index, not taughtRiding)
-			break
+
+	if(C.isBetaClient) then
+		for index, id in next, C_MountJournal.GetMountIDs() do
+			local _, spellID, _, _, _, _, _, _, _, _, collected = C_MountJournal.GetMountInfoByID(id)
+			if(spellID == 179244 and collected) then
+				C_MountJournal.SetIsFavorite(index, not taughtRiding)
+				break
+			end
+		end
+	else
+		for index = 1, C_MountJournal.GetNumMounts() do
+			local _, spellID, _, _, _, _, _, _, _, _, collected = C_MountJournal.GetMountInfo(index)
+			if(specificellID == 179244 and collected) then
+				C_MountJournal.SetIsFavorite(index, not taughtRiding)
+				break
+			end
 		end
 	end
 
