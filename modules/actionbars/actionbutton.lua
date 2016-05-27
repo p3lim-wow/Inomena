fbb5620441c7e41952d9d6279eacea8cb3bbcb55
Adrian L Lange <mail@p3lim.net>
2016-05-27 15:32:29 +0200

    actionbars/actionbutton: Add compatibility for Legion methods

--- modules/actionbars/actionbutton.lua
+++ modules/actionbars/actionbutton.lua
@@ -1,5 +1,7 @@
 local E, F, C = unpack(select(2, ...))
 
+local textureMethod = C.isBetaClient and 'SetColorTexture' or 'SetTexture'
+
 function E:PLAYER_LOGIN()
 	if(not C.ApplyCVars) then return end
 
@@ -46,7 +48,7 @@ function F:SkinActionButton(Button, petButton, leaveButton)
 
 		hooksecurefunc(Button, 'SetNormalTexture', PersistentNormalTexture)
 
-		CheckedTexture:SetTexture(0, 1/2, 1, 1/3)
+		CheckedTexture[textureMethod](CheckedTexture, 0, 1/2, 1, 1/3)
 		CheckedTexture:ClearAllPoints()
 		CheckedTexture:SetPoint('TOPRIGHT', -1, -1)
 		CheckedTexture:SetPoint('BOTTOMLEFT', 1, 1)
@@ -84,19 +86,19 @@ function F:SkinActionButton(Button, petButton, leaveButton)
 	Icon:SetAllPoints()
 
 	local PushedTexture = Button:GetPushedTexture()
-	PushedTexture:SetTexture(1, 1, 2/5, 1/5)
+	PushedTexture[textureMethod](PushedTexture, 1, 1, 2/5, 1/5)
 	PushedTexture:ClearAllPoints()
 	PushedTexture:SetPoint('TOPRIGHT', -1, -1)
 	PushedTexture:SetPoint('BOTTOMLEFT', 1, 1)
 
 	local HighlightTexture = Button:GetHighlightTexture()
-	HighlightTexture:SetTexture(0, 3/5, 1, 1/5)
+	HighlightTexture[textureMethod](HighlightTexture, 0, 3/5, 1, 1/5)
 	HighlightTexture:ClearAllPoints()
 	HighlightTexture:SetPoint('TOPRIGHT', -1, -1)
 	HighlightTexture:SetPoint('BOTTOMLEFT', 1, 1)
 
 	local Flash = Button.Flash
-	Flash:SetTexture(1, 0, 0, 1/3)
+	Flash[textureMethod](Flash, 1, 0, 0, 1/3)
 	Flash:ClearAllPoints()
 	Flash:SetPoint('TOPRIGHT', -1, -1)
 	Flash:SetPoint('BOTTOMLEFT', 1, 1)
