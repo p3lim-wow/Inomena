2b149652d4f68652ba3008fe9e532e6cfae04145
Adrian L Lange <mail@p3lim.net>
2016-05-28 16:05:09 +0200

    chat/tell: Fix missing editbox error

--- modules/chat/tell.lua
+++ modules/chat/tell.lua
@@ -1,6 +1,7 @@
 hooksecurefunc('ChatEdit_OnSpacePressed', function(self)
 	if(string.sub(self:GetText(), 1, 3) == '/tt' and (UnitCanCooperate('player', 'target') or UnitIsUnit('player', 'target'))) then
-		ChatFrame_SendTell(GetUnitName('target', true):gsub(' ', ''))
+		self:SetText(SLASH_SMART_WHISPER1 .. ' ' .. GetUnitName('target', true):gsub(' ', '') .. ' ')
+		ChatEdit_ParseText(self, 0)
 	end
 end)
 
