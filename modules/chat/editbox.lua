4a060b00530d2bd5c8ba363dd23d6051fe1982e9
Adrian L Lange <mail@p3lim.net>
2016-05-28 19:32:47 +0200

    chat/editbox: Use self as the reference

--- modules/chat/editbox.lua
+++ modules/chat/editbox.lua
@@ -1,26 +1,26 @@
 local E, F, C = unpack(select(2, ...))
 
-function ChatEdit_UpdateHeader(editbox)
-	local chatType = editbox:GetAttribute('chatType')
+function ChatEdit_UpdateHeader(self)
+	local chatType = self:GetAttribute('chatType')
 	if(not chatType) then
 		return
 	end
 
 	local info = ChatTypeInfo[chatType]
-	local header = editbox.header
+	local header = self.header
 	if(not header) then
 		return
 	end
 
-	local target = editbox:GetAttribute('tellTarget')
+	local target = self:GetAttribute('tellTarget')
 	if(chatType == 'SMART_WHISPER') then
 		if(GetAutoCompletePresenceID(target)) then
-			editbox:SetAttribute('chatType', 'BN_WHISPER')
+			self:SetAttribute('chatType', 'BN_WHISPER')
 		else
-			editbox:SetAttribute('chatType', 'WHISPER')
+			self:SetAttribute('chatType', 'WHISPER')
 		end
 
-		return ChatEdit_UpdateHeader(editbox)
+		return ChatEdit_UpdateHeader(self)
 	elseif(chatType == 'WHISPER') then
 		-- TODO: class color
 		header:SetFormattedText('|cffa1a1a1@|r%s: ', target)
@@ -31,7 +31,7 @@ function ChatEdit_UpdateHeader(editbox)
 	elseif(chatType == 'EMOTE') then
 		header:SetFormattedText(CHAT_EMOTE_SEND, YOU)
 	elseif(chatType == 'CHANNEL') then
-		local channel, channelName, instanceID = GetChannelName(editbox:GetAttribute('channelTarget'))
+		local channel, channelName, instanceID = GetChannelName(self:GetAttribute('channelTarget'))
 		if(channelName) then
 			channelName = string.match(channelName, '%w+')
 
@@ -40,32 +40,32 @@ function ChatEdit_UpdateHeader(editbox)
 			end
 
 			info = ChatTypeInfo['CHANNEL' .. channel]
-			editbox:SetAttribute('channelTarget', channel)
+			self:SetAttribute('channelTarget', channel)
 			header:SetFormattedText('%s: ', channelName)
 		end
 	elseif(chatType == 'PARTY' and (not IsInGroup(LE_PARTY_CATEGORY_HOME) and IsInGroup(LE_PARTY_CATEGORY_INSTANCE))) then
-		editbox:SetAttribute('chatType', 'INSTANCE_CHAT')
-		return ChatEdit_UpdateHeader(editbox)
+		self:SetAttribute('chatType', 'INSTANCE_CHAT')
+		return ChatEdit_UpdateHeader(self)
 	elseif(chatType == 'RAID' and (not IsInRaid(LE_PARTY_CATEGORY_HOME) and IsInRaid(LE_PARTY_CATEGORY_INSTANCE))) then
-		editbox:SetAttribute('chatType', 'INSTANCE_CHAT')
-		return ChatEdit_UpdateHeader(editbox)
+		self:SetAttribute('chatType', 'INSTANCE_CHAT')
+		return ChatEdit_UpdateHeader(self)
 	elseif(chatType == 'INSTANCE_CHAT' and (IsInGroup(LE_PARTY_CATEGORY_HOME) and not IsInGroup(LE_PARTY_CATEGORY_INSTANCE))) then
 		if(IsInRaid(LE_PARTY_CATEGORY_HOME)) then
-			editbox:SetAttribute('chatType', 'RAID')
+			self:SetAttribute('chatType', 'RAID')
 		else
-			editbox:SetAttribute('chatType', 'PARTY')
+			self:SetAttribute('chatType', 'PARTY')
 		end
 
-		return ChatEdit_UpdateHeader(editbox)
+		return ChatEdit_UpdateHeader(self)
 	else
 		header:SetText(_G['CHAT_' .. chatType .. '_SEND'])
 	end
 
-	local headerSuffix = editbox.headerSuffix
+	local headerSuffix = self.headerSuffix
 	local headerWidth = (header:GetRight() or 0) - (header:GetLeft() or 0)
-	local editboxWidth = editbox:GetRight() - editbox:GetLeft()
-	if(headerWidth > editboxWidth / 2) then
-		header:SetWidth(editboxWidth / 2)
+	local width = self:GetRight() - self:GetLeft()
+	if(headerWidth > width / 2) then
+		header:SetWidth(width / 2)
 		headerSuffix:Show()
 	else
 		headerSuffix:Hide()
@@ -73,7 +73,6 @@ function ChatEdit_UpdateHeader(editbox)
 
 	header:SetTextColor(info.r, info.g, info.b)
 	headerSuffix:SetTextColor(info.r, info.g, info.b)
-	editbox:SetTextColor(info.r, info.g, info.b)
-	editbox:SetTextInsets(header:GetWidth(), 0, 0, 0)
+	self:SetTextColor(info.r, info.g, info.b)
+	self:SetTextInsets(header:GetWidth(), 0, 0, 0)
 end
-
