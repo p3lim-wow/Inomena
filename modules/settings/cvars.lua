8b8499146c0237d7cde680bdd8b58936dbea5a92
Adrian L Lange <mail@p3lim.net>
2016-06-11 00:20:57 +0200

    settings/cvars: Update for Legion's cvars
    Screw live clients

--- modules/settings/cvars.lua
+++ modules/settings/cvars.lua
@@ -1,152 +1,135 @@
 local E, F, C = unpack(select(2, ...))
 
+-- list of all settings listed in the interface options
 local cvars = {
 	-- Controls
 	deselectOnClick = 1,
 	autoDismountFlying = 1,
 	autoClearAFK = 1,
-	blockTrades = 0,
-	blockChannelInvites = 1,
-	lootUnderMouse = 0,
 	autoLootDefault = 1,
-	autoOpenLootHistory = 0,
 	interactOnLeftClick = 0,
-	spellActivationOverlayOpacity = 1,
 
 	-- Combat
-	assistAttack = 0,
-	autoSelfCast = 1,
-	stopAutoAttackOnTargetChange = 0,
-	showVKeyCastbar = 1,
-	showVKeyCastbarOnlyOnTarget = 0,
-	showVKeyCastbarSpellName = 1,
-	displaySpellActivationOverlays = 1,
 	spellActivationOverlayOpacity = 1,
-	reducedLagTolerance = 0,
-	ActionButtonUseKeyDown = 1,
+	doNotFlashLowHealthWarning = 1,
 	lossOfControl = 1,
-	lossOfControlFull = 2,
-	lossOfControlSilence = 2,
-	lossOfControlInterrupt = 2,
-	lossOfControlDisarm = 2,
-	lossOfControlRoot = 2,
 
 	-- Display
-	threatPlaySounds = 0,
-	SpellTooltip_DisplayAvgValues = 1,
-	movieSubtitle = 1,
+	hideAdventureJournalAlerts = 0, -- legion
+	showTutorials = 0,
+	enableFloatingCombatText = 0, -- legion
 	Outline = 2,
-
-	-- Objectives
-	autoQuestWatch = 1,
-	mapFade = 1,
-	trackQuestSorting = 'proximity',
-
-	-- Social
+	ShowQuestUnitCircles = 0, -- legion, find the category
+	findYourselfMode = -1, -- legion
 	chatBubbles = 1,
 	chatBubblesParty = 0,
+
+	-- Social
 	profanityFilter = 0,
 	spamFilter = 0,
-	removeChatDelay = 1,
 	guildMemberNotify = 1,
-	chatMouseScroll = 1,
-	chatStyle = 'im',
-	showTimestamps = 'none',
-	whisperMode = 'inline',
-
-	-- Names
-	UnitNameOwn = 0,
-	UnitNameNPC = 0,
-	UnitNameForceHideMinus = 0,
-	UnitNameHostleNPC = 0,
-	UnitNameFriendlySpecialNPCName = 1,
-	UnitNameNonCombatCreatureName = 0,
-	UnitNamePlayerGuild = 1,
-	UnitNameGuildTitle = 0,
-	UnitNamePlayerPVPTitle = 0,
-	UnitNameFriendlyPlayerName = 1,
-	UnitNameFriendlyPetName = 0,
-	UnitNameFriendlyGuardianName = 0,
-	UnitNameFriendlyTotemName = 0,
-	UnitNameEnemyPlayerName = 1,
-	UnitNameEnemyPetName = 0,
-	UnitNameEnemyGuardianName = 0,
-	UnitNameEnemyTotemName = 0,
-
-	-- Floating Combat Text
-	showArenaEnemyFrames = 0,
-
-	-- Battle.net
+	blockTrades = 0,
+	blockChannelInvites = 0,
 	showToastOnline = 0,
 	showToastOffline = 0,
 	showToastBroadcast = 0,
 	showToastFriendRequest = 0,
-	showToastConversation = 0,
 	showToastWindow = 0,
-	cameraDistanceMaxFactor = 2,
+	enableTwitter = 0,
+	showTimestamps = 'none',
+	whisperMode = 'inline',
+
+	-- ActionBars
+	lockActionBars = 1,
+	countdownForCooldowns = 0,
+
+	-- Names
+	UnitNameOwn = 0,
+	UnitNameNPC = 0,
+	UnitNameHostleNPC = 1,
+	UnitNameFriendlySpecialNPCName = 1,
+	UnitNameInteractiveNPC = 0,
+	UnitNameNonCombatCreatureName = 0,
+	UnitNameFriendlyPlayerName = 1,
+	UnitNameFriendlyMinionName = 0,
+	UnitNameEnemyPlayerName = 1,
+	UnitNameEnemyMinionName = 0,
+	nameplateShowFriends = 0,
+	nameplateShowFriendlyMinions = 0,
+	nameplateShowEnemies = 1,
+	nameplateShowEnemyMinions = 0,
+	nameplateShowEnemyMinus = 1,
+	ShowNamePlateLoseAggroFlash = 1,
+	nameplateShowAll = 1,
+	nameplateShowSelf = 0,
+	NamePlateHorizontalScale = 1,
+	NamePlateVerticalScale = 1,
+	nameplateMotion = 1,
 
 	-- Camera
-	cameraBobbing = 0,
 	cameraWaterCollision = 0,
-	cameraPivot = 1,
-	cameraDistanceMaxFactor = 4,
-	cameraDistanceMax = 50,
+	cameraDistanceMaxFactor = 4, -- 1,9 or 2 is default max
 	cameraSmoothStyle = 0,
 
 	-- Mouse
+	enableMouseSpeed = 0,
 	mouseInvertPitch = 0,
-	cameraYawMoveSpeed = 180,
-	mouseSpeed = 1,
-	autointeract = 0,
-	enableWoWMouse = 0,
+	autoInteract = 0,
+	cameraYawMoveSpeed = 90,
 
-	-- Help
-	showTutorials = 0,
-	UberTooltips = 1,
-	scriptErrors = 1,
+	-- Accessibility
+	enableMovePad = 0,
+	movieSubtitle = 1,
 	colorblindMode = 0,
-	enableMovepad = 0,
+	colorblindSimulator = 0,
 
 	-- Sound
 	Sound_EnableAllSound = 1,
 	Sound_EnableSFX = 0,
 	Sound_EnableMusic = 0,
 	Sound_EnableAmbience = 0,
+	Sound_EnableDialog = 1,
+	Sound_EnableErrorSpeech = 0,
 	Sound_EnableSoundWhenGameIsInBG = 1,
 	Sound_EnableReverb = 0,
+	Sound_EnablePositionalLowPassFilter = 1,
 	Sound_EnableDSPEffects = 0,
-	Sound_NumChannels = 24,
-
-	-- Misc
-	taintLog = 1,
-	screenshotQuality = 10,
-	screenshotFormat = 'png',
+	Sound_MasterVolume = 0.2,
+	Sound_SFXVolume = 0.3,
+	Sound_MusicVolume = 0.2,
+	Sound_AmbienceVolume = 0.1,
+	Sound_DialogVolume = 0.6,
 }
 
-if(not C.isBetaClient) then
-	-- Floating Combat Text
-	cvars.CombatDamage = 1
-	cvars.CombatLogPeriodicSpells = 1
-	cvars.PetMeleeDamage = 1
-	cvars.CombatHealing = 1
-	cvars.CombatHealingAbsorbTarget = 1
-	cvars.fctSpellMechanics = 0
-	cvars.CombatDamageStyle = 2
-	cvars.enableCombatText = 0
-	cvars.enablePetBattleCombatText = 1
-
-	-- Social
-	cvars.bnWhisperMode = 'inline'
-	cvars.conversationMode = 'inline'
-
-	-- Sound
-	cvars.Sound_EnableSoftwareHRTF = 0
-end
+-- list of settings NOT listed in the interface options, but still used
+local uvars = {
+	ActionButtonUseKeyDown = 1,
+	autoQuestWatch = 1, -- unused
+	autoSelfCast = 1, -- unused, still a keybind
+	chatStyle = 'im',
+	lootUnderMouse = 0,
+	mapFade = 1,
+	removeChatDelay = 1, -- unused
+	screenshotFormat = 'png',
+	screenshotQuality = 10,
+	scriptErrors = 1,
+	taintLog = 1,
+	trackQuestSorting = 'proximity',
+}
 
 table.insert(C.Settings, function()
+	if(not C.isBetaClient) then
+		return
+	end
+
 	for key, value in next, cvars do
 		SetCVar(key, value)
 	end
 
+	for key, value in next, uvars do
+		SetCVar(key, value)
+	end
+
 	SetAutoDeclineGuildInvites(true)
+	NamePlateDriverFrame:UpdateNamePlateOptions()
 end)
