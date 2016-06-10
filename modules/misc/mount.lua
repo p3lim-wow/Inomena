8e0a5a5cddb914ac859242a4ead6cb84a600ed25
Adrian L Lange <mail@p3lim.net>
2016-06-11 00:21:27 +0200

    misc/mount: Update for Legion's new API
    Again, screw live clients

--- modules/misc/mount.lua
+++ modules/misc/mount.lua
@@ -1,27 +1,23 @@
 local E, F, C = unpack(select(2, ...))
 
-local buttonName = (...) .. 'MountButton'
-local bindingString = string.format('CLICK %s:LeftButton', buttonName)
+if(not C.isBetaClient) then
+	return
+end
 
-local DISMOUNT = [[
-/stopcasting
-/cancelform
-/leavevehicle
-/dismount
-]]
-
-local Button = CreateFrame('Button', buttonName, nil, 'SecureActionButtonTemplate')
+local Button = CreateFrame('Button', (...) .. 'MountButton', nil, 'SecureActionButtonTemplate')
 Button:SetAttribute('type', 'macro')
 
-local function HasCorralOutpost()
-	if((C.isBetaClient and HasZoneAbility or HasDraenorZoneAbility)() and not IsFlyableArea()) then
-		local ability = GetSpellInfo(161691)
-		local _, _, _, _, _, _, spellID = GetSpellInfo(ability)
-		if(spellID == 164222 or spellID == 165803) then
-			return ability
-		end
-	end
-end
+local summonMacro = [[
+/cancelform [form]
+/leavevehicle [canexitvehicle]
+/dismount [mounted]
+/run C_MountJournal.SummonByID(%s)
+]]
+
+local ownedMounts = {
+	vendor = {},
+	water = {},
+}
 
 local function Update()
 	if(InCombatLockdown()) then
@@ -31,59 +27,57 @@ local function Update()
 	end
 
 	local macro
-	if(IsAltKeyDown()) then
-		macro = '/cast Traveler\'s Tundra Mammoth'
+	if(not select(13, GetAchievementInfo(891)) and ownedMounts.taxi) then
+		macro = string.format(summonMacro, ownedMounts.taxi)
+	elseif(IsAltKeyDown()) then
+		macro = string.format(summonMacro, ownedMounts.vendor[math.random(#ownedMounts.vendor)])
 	elseif(IsShiftKeyDown()) then
-		macro = '/cast Azure Water Strider'
-	elseif(IsControlKeyDown()) then
-		-- This one is very specific to what mounts I own
-		macro = '/cast [flyable] Sandstone Drake; Mechano-Hog'
+		macro = string.format(summonMacro, ownedMounts.water[math.random(#ownedMounts.water)])
 	else
-		local spellName = HasCorralOutpost()
-		if(spellName) then
-			macro = '/cast ' .. spellName
-		else
-			if(C.isBetaClient) then
-				macro = '/run C_MountJournal.SummonByID(0)'
-			else
-				macro = '/run C_MountJournal.Summon(0)'
-			end
-		end
-	end
-
-	if(SecureCmdOptionParse('[nomod:shift]')) then
-		macro = strtrim(strjoin('\n', DISMOUNT, macro))
+		macro = string.format(summonMacro, 0)
 	end
 
 	Button:SetAttribute('macrotext', macro)
 end
 
-local function UpdateFavorites(event, newLevel)
-	local taughtRiding = IsSpellKnown(33388)
+local lastNumMounts = 0
+local function UpdateMountsList(event)
+	local collectedFlag = C_MountJournal.GetCollectedFilterSetting(1)
+	local notCollectedFlag = C_MountJournal.GetCollectedFilterSetting(2)
 
-	if(C.isBetaClient) then
-		for index, id in next, C_MountJournal.GetMountIDs() do
-			local _, spellID, _, _, _, _, _, _, _, _, collected = C_MountJournal.GetMountInfoByID(id)
-			if(spellID == 179244 and collected) then
-				C_MountJournal.SetIsFavorite(index, not taughtRiding)
-				break
-			end
-		end
+	C_MountJournal.SetCollectedFilterSetting(1, true)
+	C_MountJournal.SetCollectedFilterSetting(2, false)
+
+	local numMounts = C_MountJournal.GetNumDisplayedMounts()
+
+	C_MountJournal.SetCollectedFilterSetting(1, collectedFlag)
+	C_MountJournal.SetCollectedFilterSetting(2, notCollectedFlag)
+
+	if(numMounts == lastNumMounts) then
+		return
 	else
-		for index = 1, C_MountJournal.GetNumMounts() do
-			local _, spellID, _, _, _, _, _, _, _, _, collected = C_MountJournal.GetMountInfo(index)
-			if(specificellID == 179244 and collected) then
-				C_MountJournal.SetIsFavorite(index, not taughtRiding)
-				break
-			end
-		end
+		lastNumMounts = numMounts
 	end
 
-	if(taughtRiding) then
-		return true
+	ownedMounts.taxi = nil
+	table.wipe(ownedMounts.vendor)
+	table.wipe(ownedMounts.water)
+
+	for _, id in next, C_MountJournal.GetMountIDs() do
+		local _, _, _, _, _, _, _, _, _, ineligible, owned = C_MountJournal.GetMountInfoByID(id)
+		if(owned and not ineligible) then
+			if(id == 280 or id == 284 or id == 460) then
+				table.insert(ownedMounts.vendor, id)
+			elseif(id == 449 or id == 488) then
+				table.insert(ownedMounts.water, id)
+			elseif(id == 678 or id == 679) then
+				ownedMounts.taxi = id
+			end
+		end
 	end
 end
 
+local bindingString = string.format('CLICK %s:LeftButton', Button:GetName())
 local function SetBindings()
 	ClearOverrideBindings(Button)
 
@@ -101,7 +95,7 @@ E.UPDATE_BINDINGS = SetBindings
 E.PLAYER_ENTERING_WORLD = SetBindings
 E.PLAYER_REGEN_DISABLED = Update
 E.PLAYER_REGEN_ENABLED = Update
-E.PLAYER_LOGIN = UpdateFavorites
-E.SPELLS_CHANGED = UpdateFavorites
+E.PLAYER_LOGIN = UpdateMountsList
+E.SPELLS_CHANGED = UpdateMountsList
 
 Button:SetScript('PreClick', Update)
