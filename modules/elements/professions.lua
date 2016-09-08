4a23b0c6c15e606b49e76ff292ddb9da63cedf92
Adrian L Lange <mail@p3lim.net>
2016-09-08 07:04:16 +0200

    elements/professions: Add vellum button

--- modules/elements/professions.lua
+++ modules/elements/professions.lua
@@ -9,6 +9,57 @@ local function OnTabClick()
 	end
 end
 
+local VellumButton
+local function OnRecipesRefresh(self)
+	if(InCombatLockdown()) then
+		VellumButton:SetAlpha(0)
+		return
+	else
+		VellumButton:SetAlpha(1)
+	end
+
+	if(self.createVerbOverride and self.createVerbOverride == 'Enchant') then
+		VellumButton:Show()
+
+		local numVellums = GetItemCount(38682)
+		if(numVellums == 0 or not self.craftable) then
+			VellumButton:Disable()
+		elseif(self.craftable) then
+			VellumButton:Enable()
+		end
+
+		VellumButton:SetFormattedText('%s (%d)', VellumButton.name, numVellums)
+	else
+		VellumButton:Hide()
+	end
+end
+
+local function CombatDisabled()
+	if(TradeSkillFrame.DetailsFrame:IsVisible()) then
+		OnRecipesRefresh(TradeSkillFrame.DetailsFrame)
+	end
+end
+
+local function CreateVellumButton()
+	VellumButton = CreateFrame('Button', 'VellumButton', TradeSkillFrame.DetailsFrame, 'SecureActionButtonTemplate, MagicButtonTemplate')
+	VellumButton:SetPoint('TOPRIGHT', TradeSkillFrame.DetailsFrame.CreateButton, 'TOPLEFT')
+	VellumButton:SetSize(88, 22)
+	VellumButton.name = GetSpellInfo(162250)
+	VellumButton:SetAttribute('type', 'macro')
+	VellumButton:SetAttribute('macrotext', '/run TradeSkillFrame.DetailsFrame:Create()\n/use item:38682')
+	VellumButton:Hide()
+
+	hooksecurefunc(TradeSkillFrame.DetailsFrame, 'RefreshDisplay', OnRecipesRefresh)
+
+	if(TradeSkillFrame.DetailsFrame:IsVisible()) then
+		OnRecipesRefresh(TradeSkillFrame.DetailsFrame)
+	end
+
+	E:RegisterEvent('PLAYER_REGEN_ENABLED', CombatDisabled)
+
+	return true
+end
+
 function E:ADDON_LOADED(addonName)
 	if(addonName ~= 'Blizzard_TradeSkillUI') then
 		return
@@ -41,6 +92,15 @@ function E:ADDON_LOADED(addonName)
 
 	E:RegisterEvent('TRADE_SKILL_SHOW', OnTabClick)
 	OnTabClick()
+
+	-- vellum button
+	if(IsSpellKnown(13262)) then -- Enchanting
+		if(InCombatLockdown()) then
+			E:RegisterEvent('PLAYER_REGEN_ENABLED', CreateVellumButton)
+		else
+			CreateVellumButton()
+		end
+	end
 
 	return true
 end
