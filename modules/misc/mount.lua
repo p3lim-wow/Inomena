943f9afe439f79a0cf63b5271d97ea2a6bb56367
Adrian L Lange <mail@p3lim.net>
2016-09-23 15:19:40 +0200

    misc/mount: Add support for Majestic Elderhorn Hoof

--- modules/misc/mount.lua
+++ modules/misc/mount.lua
@@ -5,6 +5,7 @@ local MOD_VENDOR = '[mod:alt]'
 
 local STOP_MACRO = '/stopmacro [nooutdoors][combat][mounted][vehicleui]'
 local CORRAL_MACRO = '/cast [outdoors,combat,nomounted,novehicleui] %s'
+local TOY_MACRO = '/usetoy [outdoors,nocombat,nomounted,novehicleui] %s'
 
 local Button = CreateFrame('Button', (...) .. 'MountButton', nil, 'SecureActionButtonTemplate')
 Button:SetAttribute('type', 'macro')
@@ -125,6 +126,12 @@ local function PreClick()
 		end
 
 		macro = strtrim(strjoin('\n', macro, string.format(mountMacro, mountID or 0)))
+	end
+
+	-- Majestic Elderhorn Hoof, for Highmountain
+	local toyID, toyName, _, toyCollected = C_ToyBox.GetToyInfo(131900)
+	if(GetCurrentMapAreaID() == 1024 and toyCollected and (GetItemCooldown(toyID)) == 0) then
+		macro = strtrim(strjoin('\n', string.format(TOY_MACRO, toyName), macro))
 	end
 
 	Button:SetAttribute('macrotext', strtrim(strjoin('\n', DISMOUNT, macro)))
