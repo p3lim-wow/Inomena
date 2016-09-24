c173bdb36110d6e8174ced9b9331295ed85103f2
Adrian L Lange <mail@p3lim.net>
2016-09-25 00:46:34 +0200

    elements/enchanting: Split the vellum stuff into its own module

---
+++ modules/elements/enchanting.lua
@@ -0,0 +1,66 @@
+local E = unpack(select(2, ...))
+
+local VellumButton
+local function OnRecipesRefresh(self)
+	if(InCombatLockdown()) then
+		VellumButton:SetAlpha(0)
+		return
+	else
+		VellumButton:SetAlpha(1)
+	end
+
+	if(self.createVerbOverride and self.createVerbOverride == 'Enchant') then
+		VellumButton:Show()
+
+		local numVellums = GetItemCount(38682)
+		if(numVellums == 0 or not self.craftable) then
+			VellumButton:Disable()
+		elseif(self.craftable) then
+			VellumButton:Enable()
+		end
+
+		VellumButton:SetFormattedText('%s (%d)', VellumButton.name, numVellums)
+	else
+		VellumButton:Hide()
+	end
+end
+
+local function OnCombatEnded()
+	if(TradeSkillFrame.DetailsFrame:IsVisible()) then
+		OnRecipesRefresh(TradeSkillFrame.DetailsFrame)
+	end
+end
+
+local function CreateVellumButton()
+	VellumButton = CreateFrame('Button', 'VellumButton', TradeSkillFrame.DetailsFrame, 'SecureActionButtonTemplate, MagicButtonTemplate')
+	VellumButton:SetPoint('TOPRIGHT', TradeSkillFrame.DetailsFrame.CreateButton, 'TOPLEFT')
+	VellumButton:SetSize(88, 22)
+	VellumButton.name = GetSpellInfo(162250)
+	VellumButton:SetAttribute('type', 'macro')
+	VellumButton:SetAttribute('macrotext', '/run TradeSkillFrame.DetailsFrame:Create()\n/use item:38682')
+	VellumButton:Hide()
+
+	hooksecurefunc(TradeSkillFrame.DetailsFrame, 'RefreshDisplay', OnRecipesRefresh)
+
+	if(TradeSkillFrame.DetailsFrame:IsVisible()) then
+		OnRecipesRefresh(TradeSkillFrame.DetailsFrame)
+	end
+
+	E:RegisterEvent('PLAYER_REGEN_ENABLED', OnCombatEnded)
+
+	return true
+end
+
+function E:ADDON_LOADED(addonName)
+	if(addonName == 'Blizzard_TradeSkillUI') then
+		if(IsSpellKnown(13262)) then -- Enchanting
+			if(InCombatLockdown()) then
+				E:RegisterEvent('PLAYER_REGEN_ENABLED', CreateVellumButton)
+			else
+				CreateVellumButton()
+			end
+		end
+
+		return true
+	end
+end
--- modules/elements/professions.lua
+++ modules/elements/professions.lua
@@ -1,4 +1,4 @@
-local E, F = unpack(select(2, ...))
+local E = unpack(select(2, ...))
 
 local tabs = {}
 local function OnTabClick()
@@ -9,98 +9,35 @@ local function OnTabClick()
 	end
 end
 
-local VellumButton
-local function OnRecipesRefresh(self)
-	if(InCombatLockdown()) then
-		VellumButton:SetAlpha(0)
-		return
-	else
-		VellumButton:SetAlpha(1)
-	end
-
-	if(self.createVerbOverride and self.createVerbOverride == 'Enchant') then
-		VellumButton:Show()
-
-		local numVellums = GetItemCount(38682)
-		if(numVellums == 0 or not self.craftable) then
-			VellumButton:Disable()
-		elseif(self.craftable) then
-			VellumButton:Enable()
-		end
-
-		VellumButton:SetFormattedText('%s (%d)', VellumButton.name, numVellums)
-	else
-		VellumButton:Hide()
-	end
-end
-
-local function CombatDisabled()
-	if(TradeSkillFrame.DetailsFrame:IsVisible()) then
-		OnRecipesRefresh(TradeSkillFrame.DetailsFrame)
-	end
-end
-
-local function CreateVellumButton()
-	VellumButton = CreateFrame('Button', 'VellumButton', TradeSkillFrame.DetailsFrame, 'SecureActionButtonTemplate, MagicButtonTemplate')
-	VellumButton:SetPoint('TOPRIGHT', TradeSkillFrame.DetailsFrame.CreateButton, 'TOPLEFT')
-	VellumButton:SetSize(88, 22)
-	VellumButton.name = GetSpellInfo(162250)
-	VellumButton:SetAttribute('type', 'macro')
-	VellumButton:SetAttribute('macrotext', '/run TradeSkillFrame.DetailsFrame:Create()\n/use item:38682')
-	VellumButton:Hide()
-
-	hooksecurefunc(TradeSkillFrame.DetailsFrame, 'RefreshDisplay', OnRecipesRefresh)
-
-	if(TradeSkillFrame.DetailsFrame:IsVisible()) then
-		OnRecipesRefresh(TradeSkillFrame.DetailsFrame)
-	end
-
-	E:RegisterEvent('PLAYER_REGEN_ENABLED', CombatDisabled)
-
-	return true
-end
-
 function E:ADDON_LOADED(addonName)
-	if(addonName ~= 'Blizzard_TradeSkillUI') then
-		return
-	end
+	if(addonName == 'Blizzard_TradeSkillUI') then
+		for index, id in next, {GetProfessions()} do
+			if(id and index ~= 4 and index ~= 3) then -- ignore fishing and archaeology
+				local name, texture, rank, maxRank = GetProfessionInfo(id)
+				local Tab = CreateFrame('CheckButton', nil, TradeSkillFrame, 'SpellBookSkillLineTabTemplate, SecureActionButtonTemplate')
+				Tab:SetAttribute('type', 'spell')
+				Tab:SetAttribute('spell', name)
+				Tab:SetNormalTexture(texture)
+				Tab:SetID(index)
+				Tab:SetMotionScriptsWhileDisabled(true)
+				Tab:HookScript('OnClick', OnTabClick)
+				Tab.name = name
+				Tab.tooltip = string.format(ITEM_SET_NAME, name, rank, maxRank)
 
-	-- profession tabs
-	for index, id in next, {GetProfessions()} do
-		if(id and index ~= 4 and index ~= 3) then -- ignore fishing and archaeology
-			local name, texture, rank, maxRank = GetProfessionInfo(id)
-			local Tab = CreateFrame('CheckButton', nil, TradeSkillFrame, 'SpellBookSkillLineTabTemplate, SecureActionButtonTemplate')
-			Tab:SetAttribute('type', 'spell')
-			Tab:SetAttribute('spell', name)
-			Tab:SetNormalTexture(texture)
-			Tab:SetID(index)
-			Tab:SetMotionScriptsWhileDisabled(true)
-			Tab:HookScript('OnClick', OnTabClick)
-			Tab.name = name
-			Tab.tooltip = string.format(ITEM_SET_NAME, name, rank, maxRank)
+				if(#tabs == 0) then
+					Tab:SetPoint('TOPLEFT', TradeSkillFrame, 'TOPRIGHT', 0, -36)
+				else
+					Tab:SetPoint('TOPLEFT', tabs[#tabs], 'BOTTOMLEFT', 0, -17)
+				end
+				Tab:Show()
 
-			if(#tabs == 0) then
-				Tab:SetPoint('TOPLEFT', TradeSkillFrame, 'TOPRIGHT', 0, -36)
-			else
-				Tab:SetPoint('TOPLEFT', tabs[#tabs], 'BOTTOMLEFT', 0, -17)
+				tabs[#tabs + 1] = Tab
 			end
-			Tab:Show()
-
-			tabs[#tabs + 1] = Tab
 		end
+
+		E:RegisterEvent('TRADE_SKILL_SHOW', OnTabClick)
+		OnTabClick()
+
+		return true
 	end
-
-	E:RegisterEvent('TRADE_SKILL_SHOW', OnTabClick)
-	OnTabClick()
-
-	-- vellum button
-	if(IsSpellKnown(13262)) then -- Enchanting
-		if(InCombatLockdown()) then
-			E:RegisterEvent('PLAYER_REGEN_ENABLED', CreateVellumButton)
-		else
-			CreateVellumButton()
-		end
-	end
-
-	return true
 end
