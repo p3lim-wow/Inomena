697ef00a18a900b3997d3cc221b6640914eabdc5
Adrian L Lange <mail@p3lim.net>
2016-10-20 21:50:09 +0200

    misc/tweaks: Hide talking heads we've already seen

--- modules/misc/tweaks.lua
+++ modules/misc/tweaks.lua
@@ -9,6 +9,19 @@ function E:PLAYER_REGEN_DISABLED()
 	UIErrorsFrame:AddMessage('+ Combat', 1, 1, 1)
 end
 
+local function OnTalkingHeadShow(self)
+	if(not InomenaSeenHeads) then
+		InomenaSeenHeads = {}
+	end
+
+	local _, _, voiceOverID = C_TalkingHead.GetCurrentLineInfo()
+	if(InomenaSeenHeads[voiceOverID]) then
+		TalkingHeadFrame_CloseImmediately()
+	else
+		InomenaSeenHeads[voiceOverID] = true
+	end
+end
+
 function E:ADDON_LOADED(addon)
 	if(addon == 'Blizzard_GuildUI') then
 		-- Set default tab in guild window
@@ -19,6 +32,9 @@ function E:ADDON_LOADED(addon)
 		-- Hide the Class Hall bar
 		OrderHallCommandBar:Hide()
 		OrderHallCommandBar.Show = F.noop
+	elseif(addon == 'Blizzard_TalkingHeadUI') then
+		-- Only show the talking head once
+		TalkingHeadFrame:HookScript('OnShow', OnTalkingHeadShow)
 	end
 end
 
