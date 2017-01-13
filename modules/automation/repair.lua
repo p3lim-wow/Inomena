166b6b6aa897addca5ac581e590fa3f213b8e848
Adrian L Lange <mail@p3lim.net>
2017-01-13 18:49:16 +0100

    automation/repair: Fix for guild repairs being depleted

--- modules/automation/repair.lua
+++ modules/automation/repair.lua
@@ -2,6 +2,10 @@ local E, F = unpack(select(2, ...))
 
 function E:MERCHANT_SHOW()
 	if(CanMerchantRepair() and not IsShiftKeyDown()) then
-		RepairAllItems(CanGuildBankRepair() and CanWithdrawGuildBankMoney() and GetGuildBankWithdrawMoney() >= GetRepairAllCost())
+		local cost, canRepair = GetRepairAllCost()
+		if(canRepair) then
+			RepairAllItems(CanGuildBankRepair() and CanWithdrawGuildBankMoney() and GetGuildBankMoney() >= cost and GetGuildBankWithdrawMoney() >= cost)
+			PlaySound('ITEM_REPAIR')
+		end
 	end
 end
