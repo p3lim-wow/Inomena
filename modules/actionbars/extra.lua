4672d65e8b7a44f827c058eb86fc68b0ef6011ef
Adrian L Lange <mail@p3lim.net>
2017-03-03 11:26:04 +0100

    actionbars/extra: Adjust the position and remove the background

--- modules/actionbars/extra.lua
+++ modules/actionbars/extra.lua
@@ -1,7 +1,7 @@
 local E, F, C = unpack(select(2, ...))
 
 local Parent = CreateFrame('Frame', C.Name .. 'ExtraBarParent', UIParent, 'SecureHandlerStateTemplate')
-Parent:SetPoint('BOTTOM', _G[C.Name .. 'ActionBar'], 0, 240)
+Parent:SetPoint('BOTTOM', _G[C.Name .. 'ActionBar'], 0, 148)
 Parent:SetSize(50, 50)
 
 RegisterStateDriver(Parent, 'visibility', '[petbattle][overridebar][vehicleui] hide; show')
@@ -10,3 +10,5 @@ ExtraActionBarFrame:SetParent(Parent)
 ExtraActionBarFrame:EnableMouse(false)
 ExtraActionBarFrame:SetAllPoints()
 ExtraActionBarFrame.ignoreFramePositionManager = true
+
+ExtraActionButton1.style:Hide()
