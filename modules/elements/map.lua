8837513c3ee3c3c2fc2b66ab239b99ff56408aef
Adrian L Lange <mail@p3lim.net>
2017-03-03 11:27:35 +0100

    elements/map: A little cleaning

--- modules/elements/map.lua
+++ modules/elements/map.lua
@@ -1,30 +1,35 @@
 local E, F, C = unpack(select(2, ...))
 
 local CoordText
-local totalElapsed = 0
-local function UpdateCoords(self, elapsed)
-	if(totalElapsed > 0.1) then
-		if(WorldMapScrollFrame:IsMouseOver()) then
-			local scale = self:GetEffectiveScale()
-			local centerX, centerY = self:GetCenter()
-			local width, height = self:GetSize()
-			local x, y = GetCursorPosition()
+local function UpdateCoords(self)
+	if(WorldMapScrollFrame:IsMouseOver()) then
+		local scale = self:GetEffectiveScale()
+		local centerX, centerY = self:GetCenter()
+		local width, height = self:GetSize()
+		local x, y = GetCursorPosition()
 
-			x = ((x / scale) - (centerX - (width / 2))) / width
-			y = (centerY + (height / 2) - (y / scale)) / height
+		x = ((x / scale) - (centerX - (width / 2))) / width
+		y = (centerY + (height / 2) - (y / scale)) / height
 
-			CoordText:SetFormattedText('%.2f, %.2f', x * 100, y * 100)
-			CoordText:SetTextColor(0, 1, 0)
+		CoordText:SetFormattedText('%.2f, %.2f', x * 100, y * 100)
+		CoordText:SetTextColor(0, 1, 0)
+	else
+		local x, y = GetPlayerMapPosition('player')
+		if(not x or not y) then
+			CoordText:SetText(UNAVAILABLE)
+			CoordText:SetTextColor(1, 0, 0)
 		else
-			local x, y = GetPlayerMapPosition('player')
-			if(not x or not y) then
-				CoordText:SetText(UNAVAILABLE)
-				CoordText:SetTextColor(1, 0, 0)
-			else
-				CoordText:SetFormattedText('%.2f, %.2f', x * 100, y * 100)
-				CoordText:SetTextColor(1, 1, 0)
-			end
+			CoordText:SetFormattedText('%.2f, %.2f', x * 100, y * 100)
+			CoordText:SetTextColor(1, 1, 0)
 		end
+	end
+end
+
+local totalElapsed = 0
+local function OnUpdate(self, elapsed)
+	if(totalElapsed > 0.05) then
+		UpdateCoords(self)
+		-- UpdateLine(self)
 
 		totalElapsed = 0
 	else
@@ -36,5 +41,5 @@ function E:PLAYER_LOGIN()
 	CoordText = WorldMapFrameCloseButton:CreateFontString(C.Name .. 'Coordinates', nil, 'GameFontNormal')
 	CoordText:SetPoint('RIGHT', WorldMapFrameCloseButton, 'LEFT', -30, 0)
 
-	WorldMapDetailFrame:HookScript('OnUpdate', UpdateCoords)
+	WorldMapDetailFrame:HookScript('OnUpdate', OnUpdate)
 end
