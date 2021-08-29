0b7ff74b6892ef4f6488078fdae4b702394b0373
Adrian L Lange <mail@p3lim.net>
2021-08-29 10:50:18 +0200

    Move stuff around and fix the main auto-send

--- modules/automation/mail.lua
+++ modules/automation/mail.lua
@@ -9,3 +9,34 @@ function addon:UPDATE_PENDING_MAIL()
 		end
 	end
 end
+
+-- remember last recipient
+local lastReceipient
+function addon:MAIL_SEND_SUCCESS()
+	if lastReceipient then
+		SendMailNameEditBox:SetText(lastReceipient)
+		SendMailNameEditBox:ClearFocus()
+	end
+end
+
+hooksecurefunc('SendMail', function(name)
+	lastReceipient = name
+end)
+
+-- auto send when attachment limit reached
+function addon:UI_ERROR_MESSAGE(messageID)
+	if messageID == 629 then
+		SendMailMailButton:Click()
+	end
+end
+
+-- auto set subject when sending/requesting money
+local function OnTextChanged(self)
+	if self:GetText() ~= '' and SendMailSubjectEditBox:GetText() == '' then
+		SendMailSubjectEditBox:SetText(MONEY)
+	end
+end
+
+SendMailMoneyGold:HookScript('OnTextChanged', OnTextChanged)
+SendMailMoneySilver:HookScript('OnTextChanged', OnTextChanged)
+SendMailMoneyCopper:HookScript('OnTextChanged', OnTextChanged)
