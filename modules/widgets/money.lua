bda026cc411e23e5178a817642b5993d2669ac74
Adrian L Lange <mail@p3lim.net>
2021-08-29 17:46:11 +0200

    Sort the tooltip

--- modules/widgets/money.lua
+++ modules/widgets/money.lua
@@ -21,6 +21,40 @@ function addon:PLAYER_MONEY()
 	InomenaMoney[GetRealmID()][character] = GetMoney()
 end
 
+-- I'd go nuts if this data wasn't sorted
+-- http://lua-users.org/wiki/SortedIteration
+local function orderedNext(tbl, n)
+	-- get the first object in the original table
+	local key = tbl[tbl.__next]
+	if not key then
+		return
+	end
+
+	-- update the iterator index
+	tbl.__next = tbl.__next + 1
+
+	-- return the key and value of the key in the source table
+	return key, tbl.__source[key]
+end
+
+local function orderedPairs(tbl)
+	local keys = {
+		__source = tbl,
+		__next = 1,
+	}
+
+	-- create an indexed array of the assoc array's keys
+	for key in pairs(tbl) do
+		table.insert(keys, key)
+	end
+
+	-- sort that table
+	table.sort(keys)
+
+	-- return a new iterator of using the keys as a map
+	return orderedNext, keys
+end
+
 local tooltip = addon:CreateButton('Money', ContainerFrame1MoneyFrame)
 tooltip:SetAllPoints()
 tooltip:SetFrameStrata('HIGH')
@@ -30,7 +64,7 @@ tooltip:SetScript('OnEnter', function(self)
 	GameTooltip:SetPoint('BOTTOMRIGHT', self, 'TOPRIGHT')
 
 	local total = 0
-	for character, money in next, InomenaMoney[GetRealmID()] do
+	for character, money in orderedPairs(InomenaMoney[GetRealmID()]) do
 		local name, class = string.split(':', character)
 
 		local color = RAID_CLASS_COLORS[class]
