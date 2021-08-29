0a92adbe8e83d13e3b3cc6787d59c195aa093997
Adrian L Lange <mail@p3lim.net>
2021-08-29 10:32:49 +0200

    Add missing utils

---
+++ utils.lua
@@ -0,0 +1,25 @@
+local _, addon = ...
+
+local utils = {}
+function utils.ShortTime(seconds)
+	-- copy from SharedXML/TimeUtil.lua with modifications to return whole time in compact format
+	local output = ''
+	if seconds >= 86400 then
+		output = ('%s%dd'):format(output, seconds / 86400)
+		seconds = seconds % 86400
+	end
+	if seconds >= 3600 then
+		output = ('%s%dh'):format(output, seconds / 3600)
+		seconds = seconds % 3600
+	end
+	if seconds >= 60 then
+		output = ('%s%dm'):format(output, seconds / 60)
+		seconds = seconds % 60
+	end
+	if seconds > 0 then
+		output = ('%s%ds'):format(output, seconds)
+	end
+	return output
+end
+
+addon.utils = utils
