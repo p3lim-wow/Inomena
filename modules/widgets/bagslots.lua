87949a62745116f716a12c6507c613a2d7eeb693
Adrian L Lange <mail@p3lim.net>
2021-08-29 19:13:51 +0200

    Easy bag swap

---
+++ modules/widgets/bagslots.lua
@@ -0,0 +1,52 @@
+local _, addon = ...
+
+-- replace bags by dragging to the bag icon
+local function putInBag(self)
+	local id = self:GetParent():GetID() + 19
+	if CursorHasItem() and not PutItemInBag(id) then
+		ClearCursor()
+	end
+end
+
+local function pickUpBag(self)
+	local id = self:GetParent():GetID() + 19
+	if not CursorHasItem() then
+		PickupBagFromSlot(id)
+	end
+end
+
+local function onMouseUp(self, button)
+	if button == 'LeftButton' then
+		local id = self:GetParent():GetID() + 19
+		if CursorHasItem() and not PutItemInBag(id) then
+			ClearCursor()
+		elseif not CursorHasItem() then
+			PickupBagFromSlot(id)
+		end
+	else
+		self:GetParent():OnMouseDown()
+	end
+end
+
+local function onEnter(self)
+	local parent = self:GetParent()
+	parent:GetScript('OnEnter')(parent)
+end
+
+local function onLeave(self)
+	local parent = self:GetParent()
+	parent:GetScript('Onleave')(parent)
+end
+
+for index = 2, 5 do
+	local parent = _G['ContainerFrame' .. index .. 'PortraitButton']
+	local button = addon:CreateButton('BagButton' .. index, parent)
+	button:RegisterForClicks('LeftButtonUp', 'RightButtonUp')
+	button:RegisterForDrag('LeftButton')
+	button:SetAllPoints()
+	button:SetScript('OnEnter', onEnter)
+	button:SetScript('OnLeave', onLeave)
+	button:SetScript('OnMouseUp', onMouseUp)
+	button:SetScript('OnReceiveDrag', putInBag)
+	button:SetScript('OnDragStart', pickUpBag)
+end
