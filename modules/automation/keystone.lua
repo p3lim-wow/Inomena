1168f4f3948ad1707efe4f5c5eb361598d9c182f
Adrian L Lange <mail@p3lim.net>
2021-08-29 10:46:38 +0200

    Automate keystones

---
+++ modules/automation/keystone.lua
@@ -0,0 +1,18 @@
+local _, addon = ...
+
+-- automatically place keystones in the font of power
+function addon:CHALLENGE_MODE_KEYSTONE_RECEPTABLE_OPEN()
+	for bagID = BACKPACK_CONTAINER, NUM_BAG_SLOTS do
+		local slots = (container)
+		for slotID = 1, GetContainerNumSlots(bagID) do
+			local itemLink = GetContainerItemLink(bagID, slotID)
+			if itemLink and itemLink:match('|Hkeystone:') then
+				PickupContainerItem(bagID, slotID)
+				if CursorHasItem() then
+					C_ChallengeMode.SlotKeystone()
+					return
+				end
+			end
+		end
+	end
+end
