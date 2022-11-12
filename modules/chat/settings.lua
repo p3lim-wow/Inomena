a567d227d034983e14708e0a25cbcb8761994721
Adrian L Lange <git@p3lim.net>
2022-11-12 18:39:32 +0100

    chat/settings: Move to own module and fix chat settings

---
+++ modules/chat/settings.lua
@@ -0,0 +1,86 @@
+local _, addon = ...
+
+local function createChatFrame(name, ...)
+	local chatFrame = name and FCF_OpenNewWindow(name, true) or DEFAULT_CHAT_FRAME
+
+	-- wipe all default channels
+	ChatFrame_RemoveAllMessageGroups(chatFrame)
+	ChatFrame_RemoveAllChannels(chatFrame)
+	ChatFrame_ReceiveAllPrivateMessages(chatFrame)
+
+	-- change color and alpha
+	FCF_SetWindowColor(chatFrame, 0, 0, 0)
+	FCF_SetWindowAlpha(chatFrame, 0)
+
+	-- register custom channels
+	if ... then
+		for index = 1, select('#', ...) do
+			ChatFrame_AddMessageGroup(chatFrame, select(index, ...))
+		end
+	end
+
+	return chatFrame
+end
+
+local function setupChatFrames()
+	-- clear out existing setup so we have a predictable base
+	FCF_ResetChatWindows()
+
+	-- close every chat frame
+	for index = 2, NUM_CHAT_WINDOWS do
+		FCF_Close(_G['ChatFrame' .. index])
+	end
+
+	-- create new chat frames with our prefered channels
+	createChatFrame(nil, 'SYSTEM', 'SAY', 'EMOTE', 'PARTY', 'PARTY_LEADER', 'RAID', 'RAID_LEADER', 'RAID_WARNING', 'INSTANCE_CHAT', 'INSTANCE_CHAT_LEADER', 'GUILD', 'OFFICER', 'ACHIEVEMENT', 'GUILD_ACHIEVEMENT')
+	createChatFrame('Whisper', 'WHISPER', 'BN_WHISPER', 'IGNORED')
+	createChatFrame('Loot', 'LOOT', 'CURRENCY', 'MONEY', 'SKILL', 'COMBAT_FACTION_CHANGE')
+	ChatFrame_AddChannel(createChatFrame('Channels'), 'General')
+
+	-- select the default chat frame
+	FCF_SelectDockFrame(DEFAULT_CHAT_FRAME)
+
+	-- set colors
+	ChangeChatColor('OFFICER', 3/4, 1/2, 1/2)
+	ChangeChatColor('RAID', 0, 1, 4/5)
+	ChangeChatColor('RAID_LEADER', 0, 1, 4/5)
+	ChangeChatColor('RAID_WARNING', 1, 1/4, 1/4)
+	ChangeChatColor('BATTLEGROUND_LEADER', 1, 1/2, 0)
+	ChangeChatColor('PARTY_LEADER', 2/3, 2/3, 1)
+	ChangeChatColor('BN_WHISPER', 1, 1/2, 1)
+	ChangeChatColor('BN_WHISPER_INFORM', 1, 1/2, 1)
+	ChangeChatColor('INSTANCE_CHAT_LEADER', 1, 1/2, 0)
+
+	-- reload edit mode layout
+	local layoutInfo = C_EditMode.GetLayouts()
+	C_EditMode.SetActiveLayout(1) -- set one of the base layouts first
+	C_EditMode.SetActiveLayout(layoutInfo.activeLayout)
+
+	-- addon:Print('Chat frames reset')
+end
+
+-- add a quick slash to reset in case something is wrong
+addon:RegisterSlash('/chatreset', setupChatFrames)
+
+function addon:PLAYER_LOGIN()
+	-- prevent stickyness
+	ChatTypeInfo.CHANNEL.sticky = 0
+	ChatTypeInfo.WHISPER.sticky = 0
+	ChatTypeInfo.BN_WHISPER.sticky = 0
+
+	-- make more channels flash
+	ChatTypeInfo.GUILD.flashTab = true
+	ChatTypeInfo.OFFICER.flashTab = true
+
+	-- we'll never have channels in the default window, use that as an indicator for when to reset
+	if #DEFAULT_CHAT_FRAME.channelList > 0 then
+		setupChatFrames()
+	end
+end
+
+-- force enable class colors in chat
+function addon:UPDATE_CHAT_COLOR_NAME_BY_CLASS(chatType, shouldColor)
+	if not shouldColor then
+		SetChatColorNameByClass(chatType, true)
+	end
+end
--- modules/chat/style.lua
+++ modules/chat/style.lua
@@ -61,67 +61,6 @@ local function updateChatFrame(chatFrame)
 	end
 end
 
-local function createChatFrame(name, ...)
-	local chatFrame = name and FCF_OpenNewWindow(name, true) or ChatFrame1
-	ChatFrame_RemoveAllMessageGroups(chatFrame)
-	ChatFrame_RemoveAllChannels(chatFrame)
-	ChatFrame_ReceiveAllPrivateMessages(chatFrame)
-
-	FCF_SetWindowColor(chatFrame, 0, 0, 0)
-	FCF_SetWindowAlpha(chatFrame, 0)
-
-	if ... then
-		-- add every message group passed through
-		for index = 1, select('#', ...) do
-			ChatFrame_AddMessageGroup(chatFrame, select(index, ...))
-		end
-	end
-
-	return chatFrame
-end
-
-local function createChatFrames()
-	-- clear out existing setup
-	FCF_ResetChatWindows()
-
-	for index = 2, NUM_CHAT_WINDOWS do
-		-- just to make sure, there might be pet battle windows or w/e remaining
-		FCF_Close(_G['ChatFrame' .. index])
-	end
-
-	-- create new frames
-	local window = createChatFrame(nil, 'SYSTEM', 'SAY', 'EMOTE', 'PARTY', 'PARTY_LEADER', 'RAID', 'RAID_LEADER', 'RAID_WARNING', 'INSTANCE_CHAT', 'INSTANCE_CHAT_LEADER', 'GUILD', 'OFFICER', 'ACHIEVEMENT', 'GUILD_ACHIEVEMENT')
-	createChatFrame('Combat') -- we'll hide this later
-	createChatFrame('Whisper', 'WHISPER', 'BN_WHISPER', 'IGNORED')
-	createChatFrame('Loot', 'LOOT', 'CURRENCY', 'MONEY', 'SKILL', 'COMBAT_FACTION_CHANGE')
-
-	local channelFrame = createChatFrame('Channels')
-	ChatFrame_AddChannel(channelFrame, 'General')
-
-	-- reposition
-	window:ClearAllPoints()
-	window:SetPoint('BOTTOMLEFT', UIParent, 50, 50)
-	window:SetSize(400, 180)
-	FCF_SavePositionAndDimensions(window)
-	FCF_RestorePositionAndDimensions(window) -- the above position doesn't take effect
-
-	-- hide the combat frame
-	FCF_Close(ChatFrame2)
-
-	-- select the default chat frame
-	FCF_SelectDockFrame(window)
-
-	-- set colors
-	ChangeChatColor('OFFICER', 3/4, 1/2, 1/2)
-	ChangeChatColor('RAID', 0, 1, 4/5)
-	ChangeChatColor('RAID_LEADER', 0, 1, 4/5)
-	ChangeChatColor('RAID_WARNING', 1, 1/4, 1/4)
-	ChangeChatColor('BATTLEGROUND_LEADER', 1, 1/2, 0)
-	ChangeChatColor('PARTY_LEADER', 2/3, 2/3, 1)
-	ChangeChatColor('BN_WHISPER', 1, 1/2, 1)
-	ChangeChatColor('BN_WHISPER_INFORM', 1, 1/2, 1)
-	ChangeChatColor('INSTANCE_CHAT_LEADER', 1, 1/2, 0)
-end
 
 function addon:UPDATE_CHAT_WINDOWS()
 	local font, size, flags = _G[addon.FONT]:GetFont()
@@ -147,11 +86,6 @@ function addon:PLAYER_LOGIN()
 	_G.CHAT_FRAME_FADE_TIME = 0
 	_G.CHAT_FRAME_FADE_OUT_TIME = 0.5
 
-	-- create frames if they don't exist already
-	if FCF_GetChatWindowInfo(4) == '' then
-		createChatFrames()
-	end
-
 	-- skin chatframes
 	for index = 1, NUM_CHAT_WINDOWS do
 		updateChatFrame(_G['ChatFrame' .. index])
@@ -160,15 +94,6 @@ function addon:PLAYER_LOGIN()
 	-- hook into functions that would reset chat tab colors
 	hooksecurefunc('FCFTab_UpdateColors', updateChatTab)
 	hooksecurefunc('FCF_StartAlertFlash', updateChatTab)
-
-	-- change stickyness of the chat edit box
-	ChatTypeInfo.CHANNEL.sticky = 0
-	ChatTypeInfo.WHISPER.sticky = 0
-	ChatTypeInfo.BN_WHISPER.sticky = 0
-
-	-- make some channels flash
-	ChatTypeInfo.GUILD.flashTab = true
-	ChatTypeInfo.OFFICER.flashTab = true
 
 	-- remove some buttons
 	addon:Hide(QuickJoinToastButton)
@@ -179,20 +104,3 @@ function addon:PLAYER_LOGIN()
 		addon:Hide('ChatFrame' .. index .. 'ButtonFrame')
 	end
 end
-
-function addon:PLAYER_ENTERING_WORLD()
-	-- game keeps re-adding the fucking channels to chatframe 1
-	ChatFrame_RemoveAllChannels(ChatFrame1)
-end
-
-function addon:UPDATE_CHAT_COLOR_NAME_BY_CLASS(chatType, shouldColor)
-	-- force enable class colors in chat
-	if not shouldColor then
-		SetChatColorNameByClass(chatType, true)
-	end
-end
-
-hooksecurefunc('ChatEdit_UpdateHeader', function(editBox)
-	-- fix inset of chat edit header
-	editBox:SetTextInsets(editBox.header:GetWidth(), 0, 0, 0)
-end)
