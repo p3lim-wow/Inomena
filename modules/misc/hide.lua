51d3c32c95e7bff42139e55563d74582a7101562
Adrian L Lange <git@p3lim.net>
2022-11-12 18:40:12 +0100

    misc/hide: Enable and use names instead of objects

--- modules/misc/hide.lua
+++ modules/misc/hide.lua
@@ -1,11 +1,13 @@
 local _, addon = ...
 
 -- hide misc crap
-addon:Hide(TicketStatusFrame)
-addon:Hide(DurabilityFrame)
-addon:Hide(VehicleSeatIndicator)
+addon:Hide('TicketStatusFrame')
+addon:Hide('DurabilityFrame')
+addon:Hide('VehicleSeatIndicator')
+addon:Hide('StanceBar')
+addon:Hide('MainMenuBarVehicleLeaveButton')
 
 -- order hall bar
 addon:HookAddOn('Blizzard_OrderHallUI', function()
-	addon:Hide(OrderHallCommandBar)
+	addon:Hide('OrderHallCommandBar')
 end)
