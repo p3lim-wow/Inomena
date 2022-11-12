393522e361ce914a5067c9d5837ede8715a5d7d2
Adrian L Lange <git@p3lim.net>
2022-11-12 18:12:46 +0100

    automation/mail: Fix mailbox tracking for 10.0

--- modules/automation/mail.lua
+++ modules/automation/mail.lua
@@ -2,10 +2,10 @@ local _, addon = ...
 
 -- track mailboxes if we have pending mail
 function addon:UPDATE_PENDING_MAIL()
-	for index = 1, GetNumTrackingTypes() do
-		local name, _, active = GetTrackingInfo(index)
-		if name == MINIMAP_TRACKING_MAILBOX then
-			return SetTracking(index, HasNewMail() and not active)
+	for index = 1, C_Minimap.GetNumTrackingTypes() do
+		if C_Minimap.GetTrackingInfo(index) == MINIMAP_TRACKING_MAILBOX then
+			C_Minimap.SetTracking(index, HasNewMail())
+			break
 		end
 	end
 end
