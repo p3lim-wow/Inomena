0ee4fd5d12082bd3f22fad060f093be2fe7d72f0
Adrian L Lange <git@p3lim.net>
2023-03-18 16:00:50 +0100

    settings: Update cvars

--- modules/settings/cvars.lua
+++ modules/settings/cvars.lua
@@ -6,128 +6,164 @@ local CVARS = {
 	deselectOnClick = 1,
 	autoDismountFlying = 1,
 	autoClearAFK = 1,
-	autoLootDefault = 1,
 	interactOnLeftClick = 0,
+	lootUnderMouse = 0,
+	autoLootDefault = 1,
+	combinedBags = 0,
+	empowerTapControls = 0,
+	SoftTargetInteract = 1,
+	softTargettingInteractKeySound = 0,
+	ClipCursor = 0,
+	mouseInvertPitch = 0,
+	cameraPitchMoveSpeed = 45,
+	cameraYawMoveSpeed = 90,
+	enableMouseSpeed = 0,
+	autoInteract = 0,
+	cameraWaterCollision = 0,
+	cameraYawSmoothSpeed = 180,
+	cameraPitchSmoothSpeed = 45,
+	cameraSmoothStyle = 0,
 
-	-- Combat
-	spellActivationOverlayOpacity = 1,
-	doNotFlashLowHealthWarning = 1,
-	lossOfControl = 1,
-
-	-- Display
-	hideAdventureJournalAlerts = 0, -- legion
+	-- Interface
+	UnitNameOwn = 0,
+	UnitNameNPC = 0,
+	UnitNameHostleNPC = 0,
+	UnitNameFriendlySpecialNPCName = 1,
+	UnitNameInteractiveNPC = 0,
+	ShowQuestUnitCircles = 0,
+	UnitNameNonCombatCreatureName = 0,
+	UnitNameFriendlyPlayerName = 1,
+	UnitNameFriendlyMinionName = 0,
+	UnitNameEnemyPlayerName = 1,
+	UnitNameEnemyMinionName = 0,
+	nameplateShowAll = 1,
+	NamePlateHorizontalScale = 1,
+	NamePlateVerticalScale = 1,
+	NamePlateClassificationScale = 1,
+	nameplateShowEnemies = 1,
+	nameplateShowEnemyPets = 1,
+	nameplateShowEnemyGuardians = 1,
+	nameplateShowEnemyTotems = 1,
+	nameplateShowEnemyMinions = 1,
+	nameplateShowEnemyMinus = 1,
+	nameplateShowFriends = 0,
+	nameplateShowFriendlyGuardians = 0,
+	nameplateShowFriendlyTotems = 0,
+	nameplateShowFriendlyMinions = 0,
+	nameplateShowFriendlyMinus = 0,
+	ShowNamePlateLoseAggroFlash = 1,
+	nameplateMotion = 1,
+	nameplateShowOnlyNames = 0,
+	hideAdventureJournalAlerts = 1,
+	showInGameNavigation = 1,
 	showTutorials = 0,
-	enableFloatingCombatText = 0, -- legion
-	Outline = 2,
-	ShowQuestUnitCircles = 0, -- legion, find the category
-	findYourselfMode = -1, -- legion
+	Outline = 1,
+	statusText = 1,
+	statusTextDisplay = 'BOTH',
 	chatBubbles = 1,
 	chatBubblesParty = 0,
 
+	-- Action Bars
+	lockActionBars = 1,
+	countdownForCooldowns = 1,
+
+	-- Combat
+	nameplateShowSelf = 0,
+	findYourselfMode = -1,
+	doNotFlashLowHealthWarning = 1,
+	lossOfControl = 1,
+	enableFloatingCombatText = 0,
+	enableMouseoverCast = 0,
+	autoSelfCast = 1,
+	-- no cvar for self cast key? we want it set to "None"
+	-- no cvar for focus cast key? we want it set to "None"
+	spellActivationOverlayOpacity = 0.5,
+	ActionButtonUseKeyHeldSpell = 0,
+	SoftTargetEnemy = 1,
+
 	-- Social
 	profanityFilter = 0,
-	spamFilter = 0,
 	guildMemberNotify = 1,
 	blockTrades = 0,
+	-- no cvar for block guild invites?
+	-- no cvar for display only character achievements?
 	blockChannelInvites = 0,
 	showToastOnline = 0,
 	showToastOffline = 0,
 	showToastBroadcast = 0,
 	showToastFriendRequest = 0,
 	showToastWindow = 0,
-	enableTwitter = 0,
-	showTimestamps = 'none',
+	autoAcceptQuickJoinRequests = 0,
+	chatStyle = 'classic',
 	whisperMode = 'inline',
-
-	-- ActionBars
-	lockActionBars = 1,
-	alwaysShowActionBars = 0,
-	countdownForCooldowns = 0,
-
-	-- Names
-	UnitNameOwn = 0,
-	UnitNameNPC = 0,
-	UnitNameHostleNPC = 0,
-	UnitNameFriendlySpecialNPCName = 1,
-	UnitNameInteractiveNPC = 0,
-	UnitNameNonCombatCreatureName = 0,
-	UnitNameFriendlyPlayerName = 1,
-	UnitNameFriendlyMinionName = 0,
-	UnitNameEnemyPlayerName = 1,
-	UnitNameEnemyMinionName = 0,
-	nameplateShowFriends = 0,
-	nameplateShowFriendlyMinions = 0,
-	nameplateShowEnemies = 1,
-	nameplateShowEnemyMinions = 0,
-	nameplateShowEnemyMinus = 1,
-	ShowNamePlateLoseAggroFlash = 1,
-	nameplateShowAll = 1,
-	nameplateShowSelf = 0,
-	NamePlateHorizontalScale = 1,
-	NamePlateVerticalScale = 1,
-	nameplateMotion = 1,
-
-	-- Camera
-	cameraWaterCollision = 0,
-	cameraDistanceMaxZoomFactor = 2.6, -- 2.6 is default max
-	cameraSmoothStyle = 0,
-
-	-- Mouse
-	enableMouseSpeed = 0,
-	mouseInvertPitch = 0,
-	autoInteract = 0,
-	cameraYawMoveSpeed = 90,
+	showTimestamps = 'none',
+	enableTwitter = 0,
 
 	-- Accessibility
 	enableMovePad = 0,
 	movieSubtitle = 1,
+	overrideScreenFlash = 1,
+	questTextContrast = 0,
+	WorldTextMinSize = 8,
+	CameraKeepCharacterCentered = 1,
+	CameraReduceUnexpectedMovement = 1,
+	ShakeStrengthCamera = 1,
+	ShakeStrengthUI = 0,
+	cursorSizePreferred = 1,
+	SoftTargetTooltipInteract = 0,
+	SoftTargetIconEnemy = 0,
+	SoftTargetIconGameObject = 0,
+	SoftTargetIconInteract = 0,
+	SoftTargetLowPriorityIcons = 0,
 	colorblindMode = 0,
-	colorblindSimulator = 0,
+	speechToText = 0,
+	textToSpeech = 0,
+	remoteTextToSpeech = 0,
 
 	-- Sound
-	Sound_EnableAllSound = 1,
-	Sound_EnableSFX = 0,
-	Sound_EnableMusic = 0,
-	Sound_EnableAmbience = 0,
-	Sound_EnableDialog = 0,
-	Sound_EnableErrorSpeech = 0,
-	Sound_EnableSoundWhenGameIsInBG = 1,
-	Sound_EnableReverb = 0,
-	Sound_EnablePositionalLowPassFilter = 1,
-	Sound_EnableDSPEffects = 0,
-	Sound_MasterVolume = 0.2,
-	Sound_SFXVolume = 0.3,
-	Sound_MusicVolume = 0.2,
-	Sound_AmbienceVolume = 0.1,
-	Sound_DialogVolume = 0.6,
+	-- Sound_EnableAllSound = 1,
+	-- Sound_MasterVolume = 0.2,
+	-- Sound_SFXVolume = 0.3,
+	-- Sound_MusicVolume = 0.2,
+	-- Sound_AmbienceVolume = 0.1,
+	-- Sound_DialogVolume = 0.6,
+	-- Sound_EnableMusic = 0,
+	-- Sound_EnableSFX = 0,
+	-- Sound_EnableDialog = 0,
+	-- Sound_EnableAmbience = 0,
+	-- Sound_EnableSoundWhenGameIsInBG = 1,
+	-- Sound_EnableReverb = 1,
+	-- Sound_EnablePositionalLowPassFilter = 1,
+	VoiceOutputVolume = 0,
+	VoiceChatMasterVolumeScale = 1,
+	VoiceInputVolume = 0,
+	VoiceVADSensitivity = 0,
+	VoiceCommunicationMode = 0,
 }
 
 -- list of settings NOT listed in the interface options, but still used
 local UVARS = {
 	ActionButtonUseKeyDown = 1,
-	autoQuestWatch = 1, -- unused
-	autoSelfCast = 1, -- unused, still a keybind
-	chatStyle = 'im',
-	lootUnderMouse = 0,
-	mapFade = 1,
-	removeChatDelay = 1, -- unused
+	-- autoQuestWatch = 1, -- unused
+	-- mapFade = 1,
+	-- removeChatDelay = 1, -- unused
 	screenshotFormat = 'png',
 	screenshotQuality = 10,
 	scriptErrors = 1,
-	taintLog = 1,
-	trackQuestSorting = 'proximity',
-	secureAbilityToggle = 1,
-	cursorSizePreferred = 1,
-
-	nameplateOtherTopInset = -1,
-	nameplateOtherBottomInset = -1,
+	taintLog = 2,
+	-- trackQuestSorting = 'proximity',
+	-- secureAbilityToggle = 1,
+	-- nameplateOtherTopInset = -1,
+	-- nameplateOtherBottomInset = -1,
+	minimapTrackingShowAll = 1, -- to get the full minimap tracking menu back
+	AutoPushSpellToActionBar = 0,
 }
 
 function addon:PLAYER_LOGIN()
-	if C_CVar.GetCVarBool('autoLootDefault') then
-		-- we'll use this one to check if we're set, since it's off by default
-		return
-	end
+	-- if C_CVar.GetCVarBool('autoLootDefault') then
+	-- 	-- we'll use this one to check if we're set, since it's off by default
+	-- 	return
+	-- end
 
 	for key, value in next, CVARS do
 		C_CVar.SetCVar(key, tostring(value))
@@ -136,9 +172,6 @@ function addon:PLAYER_LOGIN()
 	for key, value in next, UVARS do
 		C_CVar.SetCVar(key, tostring(value))
 	end
-
-	-- need to update the nameplate driver for cvars to take effect
-	NamePlateDriverFrame:UpdateNamePlateOptions()
 
 	return true
 end
