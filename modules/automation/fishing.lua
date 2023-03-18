ca8a632868f200ab1a8d57c44277e439a8c7cf63
Adrian L Lange <git@p3lim.net>
2023-03-18 16:04:59 +0100

    automation: Add smart fishing rebinding/sound
    This uses the new interact key functionality to replace the binding for
    the fishing action with interacting with the bobber while fishing, also
    enabling sounds.

---
+++ modules/automation/fishing.lua
@@ -0,0 +1,149 @@
+local _, addon = ...
+
+-- while fishing the button bound to the fishing spell will be rebound to interact with the bobber,
+-- and sounds are toggled to listen to the bobber while fishing
+
+local CVARS = {
+	Sound_EnableSFX = 1,
+	Sound_MasterVolume = 1,
+	Sound_MusicVolume = 0,
+	Sound_AmbienceVolume = 0,
+	Sound_SFXVolume = 1,
+	SoftTargetInteract = 3, -- enables interacting with the fishing bobber
+	SoftTargetInteractArc = 2, -- widens interact arc
+	SoftTargetInteractRange = 30, -- increases interact range
+	SoftTargetIconInteract = 1, -- show interact icons
+	SoftTargetIconGameObject = 1, -- show interact icon specifically for objects, like the bobber
+}
+
+local FISHING_SPELLS = {
+	[131474] = true, -- cast spell
+	[131476] = true, -- cast spell
+	[131490] = true, -- channel spell
+}
+
+-- our state and attribute handler
+local handler = CreateFrame('Frame', nil, nil, 'SecureHandlerStateTemplate, SecureHandlerAttributeTemplate')
+
+-- reset override bindings if entering combat
+handler:SetAttribute('_onattributechanged', [[
+	if name == 'state-combat' and value == 'clear' then
+		self:ClearBindings()
+	end
+]])
+
+local getSpellBinding
+do
+	local function findSpellButton(spellID)
+		local slotBindings = {}
+
+		-- use existing utilities to iterate through all action buttons to find the spell
+		local button = ActionButtonUtil.GetActionButtonBySpellID(spellID)
+		if button then
+			local slot = button.action
+			local actionType, actionID = GetActionInfo(slot)
+			if (actionType == 'spell' and actionID == spellID) or (actionType == 'macro' and GetMacroSpell(actionID) == spellID) then
+				-- found a button, let's find the binding
+				local bindingName
+				local index = ((slot % 12) == 0 and 12 or (slot % 12))
+				if not button.buttonType or button.buttonType == 'ACTIONBUTTON' then
+					-- handle pages and forms
+					local page
+					if HasBonusActionBar() and GetActionBarPage() == 1 then
+						page = GetBonusBarIndex()
+					else
+						page = GetActionBarPage()
+					end
+
+					if ((page * 12) - 12 + index) == slot then
+						bindingName = 'ACTIONBUTTON' .. index
+					end
+				else
+					bindingName = button.buttonType .. index
+				end
+
+				table.insert(slotBindings, bindingName)
+			end
+		end
+
+		return slotBindings
+	end
+
+	function getSpellBinding(spellID)
+		for _, binding in next, findSpellButton(spellID) do
+			local key1, key2 = GetBindingKey(binding)
+			if key1 or key2 then
+				return key1, key2
+			end
+		end
+	end
+end
+
+local origCVars = {}
+local activeFishingSpell
+
+local function restoreCVars()
+	for name, value in next, origCVars do
+		C_CVar.SetCVar(name, value)
+	end
+end
+
+local function fishingStop(_, _, _, spellID)
+	if FISHING_SPELLS[spellID] then
+		-- restore cvars to their previous values
+		restoreCVars()
+
+		-- clear bindings
+		addon:Defer(ClearOverrideBindings, handler)
+		addon:Defer(UnregisterStateDriver, handler, 'combat')
+
+		-- unregister ourselves
+		addon:UnregisterEvent('PLAYER_LOGOUT', restoreCVars)
+		return true
+	end
+end
+
+local function fishingStart(_, _, _, spellID)
+	if FISHING_SPELLS[spellID] then
+		-- iterate through the CVars we want to change
+		for name, value in next, CVARS do
+			-- store the original values
+			origCVars[name] = C_CVar.GetCVar(name)
+
+			-- set our values
+			C_CVar.SetCVar(name, value)
+		end
+
+		-- rebind the fishing spell hotkey
+		local key1, key2 = getSpellBinding(activeFishingSpell)
+		if key1 then
+			SetOverrideBinding(handler, true, key1, 'INTERACTTARGET')
+		end
+		if key2 then
+			SetOverrideBinding(handler, true, key2, 'INTERACTTARGET')
+		end
+
+		-- register state driver to reset the binding when player enters combat
+		RegisterStateDriver(handler, 'combat', '[combat] clear; nothing')
+
+		-- wait for fishing to end
+		addon:RegisterUnitEvent('UNIT_SPELLCAST_CHANNEL_STOP', 'player', fishingStop)
+		addon:RegisterEvent('PLAYER_LOGOUT', restoreCVars)
+
+		-- unregister ourselves
+		return true
+	end
+end
+
+function addon:UNIT_SPELLCAST_SENT(unit, _, _, spellID)
+	-- BUG: this event is not a valid unit event for whatever reason
+	if unit == 'player' and FISHING_SPELLS[spellID] then
+		-- this event triggers before UNIT_SPELLCAST_CHANNEL_START and holds the actual spell
+		-- used to start fishing, so we'll store it
+		activeFishingSpell = spellID
+
+		-- wait for the channel event to trigger
+		self:RegisterUnitEvent('UNIT_SPELLCAST_CHANNEL_START', unit, fishingStart)
+	end
+end
+
