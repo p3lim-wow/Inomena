93c57580ebb6aeee45098fcc5c70aa7e4efc7edd
Adrian L Lange <git@p3lim.net>
2023-10-31 21:08:01 +0100

    misc/tracking: Auto-track certain minimap POIs

---
+++ modules/misc/tracking.lua
@@ -0,0 +1,21 @@
+local _, addon = ...
+
+-- textures for each tracking kind should be unique
+local TRACKING = {
+	[136456] = true, -- Flight Master
+	[136458] = true, -- Innkeeper
+	[524052] = true, -- Target
+	[524051] = true, -- Focus Target
+	[535616] = true, -- Track Quest POIs
+}
+
+if addon.PLAYER_CLASS == 'HUNTER' then
+	TRACKING[136466] = true -- Stable Master
+end
+
+function addon:PLAYER_LOGIN()
+	for index = 1, C_Minimap.GetNumTrackingTypes() do
+		local _, texture = C_Minimap.GetTrackingInfo(index)
+		C_Minimap.SetTracking(index, TRACKING[texture] or false)
+	end
+end
