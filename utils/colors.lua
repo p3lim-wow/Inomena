d69867c2bb71bf3a4a30cedf6094b2c5d83f3147
Adrian L Lange <git@p3lim.net>
2024-11-02 17:51:17 +0100

    Use dashi for colors and fix class colors

--- utils/colors.lua
+++ utils/colors.lua
@@ -1,19 +1,11 @@
 local _, addon = ...
 
-local function CreateColor(r, g, b)
-	-- handy conversion of 255 color space
-	if r > 1 or g > 1 or b > 1 then
-		return _G.CreateColor(r / 255, g / 255, b / 255)
-	end
-	return _G.CreateColor(r, g, b)
-end
-
 addon.colors = {}
 addon.colors.power = {}
 for powerTypeName, color in next, _G.PowerBarColor do
 	local powerType = addon.POWER_NAME_TYPE[powerTypeName]
 	if powerType then
-		addon.colors.power[powerType] = CreateColor(color.r, color.g, color.b)
+		addon.colors.power[powerType] = addon:CreateColor(color.r, color.g, color.b)
 
 		-- add the atlases too
 		if color.atlas then
@@ -23,24 +15,27 @@ for powerTypeName, color in next, _G.PowerBarColor do
 end
 
 -- replace some default colors I don't agree with
-addon.colors.power[Enum.PowerType.Mana] = CreateColor(0, 144, 255)
-addon.colors.power[Enum.PowerType.ArcaneCharges] = CreateColor(186, 77, 188)
-addon.colors.power[Enum.PowerType.SoulShards] = CreateColor(135, 136, 238) -- TODO: maybe use green color if learned green fire
+addon.colors.power[Enum.PowerType.Mana] = addon:CreateColor(0, 144, 255)
+addon.colors.power[Enum.PowerType.ArcaneCharges] = addon:CreateColor(186, 77, 188)
+addon.colors.power[Enum.PowerType.SoulShards] = addon:CreateColor(135, 136, 238) -- TODO: maybe use green color if learned green fire
 
 -- add missing power colors, typically because blizzard uses textures instead
-addon.colors.power[Enum.PowerType.Essence] = CreateColor(1, 1, 1) -- TODO
-addon.colors.power[Enum.PowerType.RuneBlood] = CreateColor(247, 65, 57)
-addon.colors.power[Enum.PowerType.RuneFrost] = CreateColor(148, 203, 247) -- TODO: too light
-addon.colors.power[Enum.PowerType.RuneUnholy] = CreateColor(173, 235, 66)
+addon.colors.power[Enum.PowerType.Essence] = addon:CreateColor(1, 1, 1) -- TODO
+addon.colors.power[Enum.PowerType.RuneBlood] = addon:CreateColor(247, 65, 57)
+addon.colors.power[Enum.PowerType.RuneFrost] = addon:CreateColor(148, 203, 247) -- TODO: too light
+addon.colors.power[Enum.PowerType.RuneUnholy] = addon:CreateColor(173, 235, 66)
 
 addon.colors.school = {
-	Curse = CreateColor(0.8, 0, 1),
-	Disease = CreateColor(0.8, 0.6, 0),
-	Magic = CreateColor(0, 0.8, 1),
-	Poison = CreateColor(0, 0.8, 0),
+	Curse = addon:CreateColor(0.8, 0, 1),
+	Disease = addon:CreateColor(0.8, 0.6, 0),
+	Magic = addon:CreateColor(0, 0.8, 1),
+	Poison = addon:CreateColor(0, 0.8, 0),
 }
 
 addon.colors.class = {}
-for className in next, _G.RAID_CLASS_COLORS do
-	addon.colors.class[className] = C_ClassColor.GetClassColor(className)
+for index = 1, GetNumClasses() do
+	local _, classToken = GetClassInfo(index)
+	if classToken then -- can be nil apparently
+			addon.colors.class[classToken] = C_ClassColor.GetClassColor(classToken)
+	end
 end
