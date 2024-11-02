b141ceeda5b846ec45753e36fa7c1a0bf1aaf90b
Adrian L Lange <git@p3lim.net>
2024-11-02 18:05:05 +0100

    chat/settings: Rewrite
    This is way more stable, and requires less input from the player.
    Still need to deal with positioning in EM

--- modules/chat/settings.lua
+++ modules/chat/settings.lua
@@ -1,31 +1,84 @@
 local _, addon = ...
 
-local NUM_CHAT_WINDOWS = _G.NUM_CHAT_WINDOWS or 10 -- FrameXML/ChatFrame.lua
-local DEFAULT_CHAT_FRAME = _G.DEFAULT_CHAT_FRAME -- FrameXML/ChatFrame.lua
+local CHAT_FRAMES = {
+	'General',
+	'Whisper',
+	'Loot',
+	'Channels',
+}
 
-local function createChatFrame(name, ...)
-	local chatFrame = name and FCF_OpenNewWindow(name, true) or DEFAULT_CHAT_FRAME
+local CHAT_GROUPS = {
+	{
+		'SYSTEM',
+		'SAY',
+		'EMOTE',
+		'PARTY',
+		'PARTY_LEADER',
+		'RAID',
+		'RAID_LEADER',
+		'RAID_WARNING',
+		'INSTANCE_CHAT',
+		'INSTANCE_CHAT_LEADER',
+		'GUILD',
+		'OFFICER',
+		'ACHIEVEMENT',
+		'GUILD_ACHIEVEMENT',
+	},
+	{
+		'WHISPER',
+		'BN_WHISPER',
+		'IGNORED'
+	},
+	{
+		'LOOT',
+		'CURRENCY',
+		'MONEY',
+		'SKILL',
+		'COMBAT_FACTION_CHANGE'
+	},
+}
 
-	-- wipe all default channels
-	ChatFrame_RemoveAllMessageGroups(chatFrame)
-	ChatFrame_RemoveAllChannels(chatFrame)
-	ChatFrame_ReceiveAllPrivateMessages(chatFrame)
+local CHAT_CHANNELS = {
+	[4] = {
+		'General'
+	},
+}
+
+local function createChatFrame(index, name)
+	local chatFrame
+	if index == 1 then
+		chatFrame = DEFAULT_CHAT_FRAME
+	else
+		chatFrame = FCF_OpenNewWindow(name, true)
+	end
 
 	-- change color and alpha
 	FCF_SetWindowColor(chatFrame, 0, 0, 0)
 	FCF_SetWindowAlpha(chatFrame, 0)
 
-	-- register custom channels
-	if ... then
-		for index = 1, select('#', ...) do
-			ChatFrame_AddMessageGroup(chatFrame, select(index, ...))
-		end
-	end
-
 	return chatFrame
 end
 
-local function setupChatFrames()
+local function registerChannels(chatFrame, tabIndex)
+	-- wipe all default channels
+	ChatFrame_RemoveAllMessageGroups(chatFrame)
+	ChatFrame_RemoveAllChannels(chatFrame)
+	ChatFrame_ReceiveAllPrivateMessages(chatFrame)
+
+	if CHAT_GROUPS[tabIndex] then
+		for _, group in next, CHAT_GROUPS[tabIndex] do
+			ChatFrame_AddMessageGroup(chatFrame, group)
+		end
+	end
+
+	if CHAT_CHANNELS[tabIndex] then
+		for _, channel in next, CHAT_CHANNELS[tabIndex] do
+			ChatFrame_AddChannel(chatFrame, channel)
+		end
+	end
+end
+
+local function setupChatFrames(tabIndex)
 	-- clear out existing setup so we have a predictable base
 	FCF_ResetChatWindows()
 
@@ -34,30 +87,22 @@ local function setupChatFrames()
 		FCF_Close(_G['ChatFrame' .. index])
 	end
 
-	-- create new chat frames with our prefered channels
-	createChatFrame(nil, 'SYSTEM', 'SAY', 'EMOTE', 'PARTY', 'PARTY_LEADER', 'RAID', 'RAID_LEADER', 'RAID_WARNING', 'INSTANCE_CHAT', 'INSTANCE_CHAT_LEADER', 'GUILD', 'OFFICER', 'ACHIEVEMENT', 'GUILD_ACHIEVEMENT')
-	createChatFrame('Whisper', 'WHISPER', 'BN_WHISPER', 'IGNORED')
-	createChatFrame('Loot', 'LOOT', 'CURRENCY', 'MONEY', 'SKILL', 'COMBAT_FACTION_CHANGE')
-	ChatFrame_AddChannel(createChatFrame('Channels'), 'General')
+	for tabIndex, name in next, CHAT_FRAMES do
+		local chatFrame = createChatFrame(tabIndex, name)
+		registerChannels(chatFrame, tabIndex)
+	end
 
 	-- select the default chat frame
 	FCF_SelectDockFrame(DEFAULT_CHAT_FRAME)
 
-	-- set colors
-	ChangeChatColor('OFFICER', 3/4, 1/2, 1/2)
-	ChangeChatColor('RAID', 0, 1, 4/5)
-	ChangeChatColor('RAID_LEADER', 0, 1, 4/5)
-	ChangeChatColor('RAID_WARNING', 1, 1/4, 1/4)
-	ChangeChatColor('BATTLEGROUND_LEADER', 1, 1/2, 0)
-	ChangeChatColor('PARTY_LEADER', 2/3, 2/3, 1)
-	ChangeChatColor('BN_WHISPER', 1, 1/2, 1)
-	ChangeChatColor('BN_WHISPER_INFORM', 1, 1/2, 1)
-	ChangeChatColor('INSTANCE_CHAT_LEADER', 1, 1/2, 0)
-
 	-- reload edit mode layout
-	local layoutInfo = C_EditMode.GetLayouts()
-	C_EditMode.SetActiveLayout(1) -- set one of the base layouts first
-	C_EditMode.SetActiveLayout(layoutInfo.activeLayout)
+	if addon:IsRetail() then
+		local layoutInfo = C_EditMode.GetLayouts()
+		C_EditMode.SetActiveLayout(1) -- set one of the base layouts first
+		C_EditMode.SetActiveLayout(layoutInfo.activeLayout)
+	else
+		-- TODO: position (bottomleft, 32, 32), size (500x246)
+	end
 
 	addon:Print('Chat frames reset, everything is tainted, you should /reload')
 end
@@ -65,25 +110,42 @@ end
 -- add a quick slash to reset in case something is wrong
 addon:RegisterSlash('/chatreset', setupChatFrames)
 
-function addon:PLAYER_LOGIN()
-	-- prevent stickyness
-	ChatTypeInfo.CHANNEL.sticky = 0
-	ChatTypeInfo.WHISPER.sticky = 0
-	ChatTypeInfo.BN_WHISPER.sticky = 0
+function addon:PLAYER_ENTERING_WORLD(initialLogin)
+	local tabIndex = 1
+	for chatFrameIndex = 1, NUM_CHAT_WINDOWS do
+		local frame = _G['ChatFrame' .. chatFrameIndex]
 
-	-- make more channels flash
-	ChatTypeInfo.GUILD.flashTab = true
-	ChatTypeInfo.OFFICER.flashTab = true
+		if chatFrameIndex == 2 or chatFrameIndex == 3 then
+			-- these two frames are reserved for "Combat Log" and "Voice",
+			-- and we need to ignore them
+		else
+			local name, _, _, _, _, _, _, _, docked = FCF_GetChatWindowInfo(chatFrameIndex)
+			if docked and name ~= CHAT_FRAMES[tabIndex] then
+				-- we have a chatframe that differs from our presets, let's reset and set up
+				setupChatFrames(tabIndex)
+				return -- no point checking more frames
+			end
 
-	-- we'll never have channels in the default window, use that as an indicator for when to reset
-	if #DEFAULT_CHAT_FRAME.channelList > 0 then
-		setupChatFrames()
+			tabIndex = tabIndex + 1
+		end
 	end
-end
 
--- force enable class colors in chat
-function addon:UPDATE_CHAT_COLOR_NAME_BY_CLASS(chatType, shouldColor)
-	if not shouldColor then
-		SetChatColorNameByClass(chatType, true)
+	if initialLogin then
+		-- BUG: currently the channels aren't staying between logins, so we need to set up groups
+		--      and channels on every login, hopefully without any taints
+		tabIndex = 1
+		for chatFrameIndex = 1, NUM_CHAT_WINDOWS do
+			if chatFrameIndex == 2 or chatFrameIndex == 3 then
+				-- ignore
+			else
+				local name, _, _, _, _, _, _, _, docked = FCF_GetChatWindowInfo(chatFrameIndex)
+				if docked and name == CHAT_FRAMES[tabIndex] then
+					-- TODO: this doesn't seem to work?
+					registerChannels(_G['ChatFrame' .. chatFrameIndex], tabIndex)
+				end
+
+				tabIndex = tabIndex + 1
+			end
+		end
 	end
 end
