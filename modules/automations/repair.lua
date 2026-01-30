4d40492e51605df64d55051e3e8794e3974a42de
Adrian L Lange <git@p3lim.net>
2026-01-30 04:02:01 +0100

    Use existing money formatter

--- modules/automations/repair.lua
+++ modules/automations/repair.lua
@@ -6,24 +6,6 @@ local SPENT_SPLIT = 'Repaired for: %s (%s using guild funds)'
 local SPENT_GUILD = 'Repaired for: %s with guild funds'
 local SPENT_POCKET = 'Repaired for: %s'
 
-local function formatCoin(coin, value)
-	return CreateAtlasMarkup('Coin-' .. coin, 10, 10) .. FormatLargeNumber(value)
-end
-
-local function formatMoney(copper)
-	local str = ''
-	if copper >= COPPER_PER_GOLD then
-		str = str .. formatCoin('Gold', math.floor(copper / COPPER_PER_GOLD)) .. ' '
-	end
-	if copper >= COPPER_PER_SILVER then
-		str = str .. formatCoin('Silver', math.floor((copper / SILVER_PER_GOLD) % COPPER_PER_SILVER)) .. ' '
-	end
-	if copper > 0 then
-		str = str .. formatCoin('Copper', copper % COPPER_PER_SILVER) .. ' '
-	end
-	return str:sub(1, -2)
-end
-
 local function chatMessage(msg)
 	local info = ChatTypeInfo.SYSTEM
 	DEFAULT_CHAT_FRAME:AddMessage(msg, info.r, info.g, info.b, info.id)
@@ -42,12 +24,12 @@ function addon:MERCHANT_SHOW()
 			local spent = money - GetMoney()
 			if spent > 0 then
 				if spent ~= cost then
-					chatMessage(SPENT_SPLIT:format(formatMoney(spent), formatMoney(cost - spent)))
+					chatMessage(SPENT_SPLIT:format(GetMoneyString(spent), GetMoneyString(cost - spent)))
 				else
-					chatMessage(SPENT_POCKET:format(formatMoney(spent)))
+					chatMessage(SPENT_POCKET:format(GetMoneyString(spent)))
 				end
 			else
-				chatMessage(SPENT_GUILD:format(formatMoney(cost)))
+				chatMessage(SPENT_GUILD:format(GetMoneyString(cost)))
 			end
 		end
 	end
