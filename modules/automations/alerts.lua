8869e6eb49ea6fbb3092b4ed03dc6e8c8a9eb4fc
Adrian L Lange <git@p3lim.net>
2026-02-03 21:44:13 +0100

    Generalize alerts and use new API

--- modules/automations/alerts.lua
+++ modules/automations/alerts.lua
@@ -1,47 +1,34 @@
 local _, addon = ...
 
--- (sound) alerts to notify me of certain things (on the master channel)
-
-local lastAlertTime = 0
-local function alert()
-	if GetTime() >= lastAlertTime + 10 then -- avoid spam
-		lastAlertTime = GetTime()
-
-		PlaySound(SOUNDKIT.READY_CHECK, 'master')
-		-- FlashClientIcon()
-	end
-end
+-- (sound) alerts to notify me of certain things
 
 -- battleground queue pop
 function addon:UPDATE_BATTLEFIELD_STATUS(index)
 	if GetBattlefieldStatus(index) == 'confirm' then
-		alert()
+		C_Sound.PlaySound(SOUNDKIT.READY_CHECK, 'master', true)
 	end
 end
 
 -- lfg queue pop
 function addon:LFG_LIST_APPLICATION_STATUS_UPDATED(_, status)
 	if status == 'invited' then
-		alert()
+		C_Sound.PlaySound(SOUNDKIT.READY_CHECK, 'master', true)
 	end
 end
 
--- readychecks
-function addon:READY_CHECK()
-	alert()
 end
 
--- invites
-addon:RegisterEvent('PARTY_INVITE_REQUEST', alert)
-addon:RegisterEvent('LFG_PROPOSAL_SHOW', alert)
-
--- whisper notifications on the master channel
-function addon:CHAT_MSG_WHISPER()
-	PlaySound(SOUNDKIT.TELL_MESSAGE, 'master')
-end
-
-function addon:CHAT_MSG_BN_WHISPER()
-	PlaySound(SOUNDKIT.TELL_MESSAGE, 'master')
+-- misc events we straight up trigger sounds for
+for event, soundkitID in next, {
+	CHAT_MSG_WHISPER = 'TELL_MESSAGE',
+	CHAT_MSG_BN_WHISPER = 'TELL_MESSAGE',
+	PARTY_INVITE_REQUEST = 'READY_CHECK',
+	READY_CHECK = 'READY_CHECK',
+	LFG_PROPOSAL_SHOW = 'READY_CHECK',
+} do
+	addon:RegisterEvent(event, function()
+		C_Sound.PlaySound(SOUNDKIT[soundkitID], 'master', true)
+	end)
 end
 
 -- display combat state changes
