f9c711001485ca5f894ad8024c7e91bfcf498d4f
Adrian L Lange <git@p3lim.net>
2026-02-06 16:35:23 +0100

    Hide all other minimap buttons

--- modules/compat/minimap.lua
+++ modules/compat/minimap.lua
@@ -1,3 +1,19 @@
+local _, addon = ...
+
+-- disable addon minimap buttons
+function addon:OnLogin()
+	local LDBI = LibStub and LibStub('LibDBIcon-1.0', true)
+	if LDBI then
+		for _, buttonName in next, LDBI:GetButtonList() do
+			addon:Hide('LibDBIcon10_' .. buttonName)
+		end
+
+		LDBI.RegisterCallback(self, 'LibDBIcon_IconCreated', function(_, _, buttonName)
+			addon:Hide('LibDBIcon10_' .. buttonName)
+		end)
+	end
+end
+
 -- expose shape for other addons
 function GetMinimapShape()
 	return 'SQUARE'
