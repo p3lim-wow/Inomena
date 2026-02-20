1cffa6053e657e47706a1878a2edc7a89f21b48d
Adrian L Lange <git@p3lim.net>
2026-02-20 20:34:10 +0100

    Get rid of the fancy health bar colors
    My eyes have a hard time reacting to castbars with all these colors,
    but we still want to distinguish the mob types somehow..

--- modules/units/elements/health.lua
+++ modules/units/elements/health.lua
@@ -1,7 +1,6 @@
 local _, addon = ...
 
 function addon.unitShared.UpdateColorHealth(self, _, unit)
-	local element = self.Health
 	if not unit or self.unit ~= unit then
 		return
 	end
@@ -10,27 +9,6 @@ function addon.unitShared.UpdateColorHealth(self, _, unit)
 	if UnitIsPlayer(unit) or UnitTreatAsPlayerForDisplay(unit) then
 		local _, classToken = UnitClass(unit)
 		color = self.colors.class[classToken]
-	elseif addon:IsInDungeon() and not addon:IsInRaid() then
-		local classification = UnitClassification(unit)
-		if classification == 'elite' or classification == 'rareelite' then
-			local playerLevel = UnitEffectiveLevel('player')
-			local level = UnitEffectiveLevel(unit)
-			if level >= (playerLevel + 2) then
-				color = addon.colors.creature.boss
-			elseif level >= (playerLevel + 1) then
-				color = addon.colors.creature.lieutenant
-			elseif level >= playerLevel then
-				if UnitClassBase(unit) == 'PALADIN' then
-					color = addon.colors.creature.caster
-				else
-					color = addon.colors.creature.melee
-				end
-			else
-				color = addon.colors.creature.trivial
-			end
-		else
-			color = addon.colors.creature.trivial
-		end
 	elseif UnitIsTapDenied(unit) then
 		color = self.colors.tapped
 	else
@@ -38,6 +16,6 @@ function addon.unitShared.UpdateColorHealth(self, _, unit)
 	end
 
 	if color then
-		element:SetStatusBarColor(color:GetRGB())
+		self.Health:SetStatusBarColor(color:GetRGB())
 	end
 end
