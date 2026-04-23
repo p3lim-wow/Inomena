45e0406114cd3faf9c7a78f6f2fee582be9fe1d4
Adrian L Lange <git@p3lim.net>
2026-04-23 18:39:29 +0200

    Can't display speed any more

--- modules/blizzard/character.lua
+++
@@ -1,8 +0,0 @@
-
--- add movement speed back to the CharacterFrame, no clue why this was removed
-
-hooksecurefunc('PaperDollFrame_SetMovementSpeed', function(self)
-	self:Show()
-end)
-
-table.insert(PAPERDOLL_STATCATEGORIES[1].stats, {stat = 'MOVESPEED'})
