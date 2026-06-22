663814f4391670a9a97958c24e27e97750f5473c
Adrian L Lange <git@p3lim.net>
2026-06-22 18:34:14 +0200

    Automate travel form for druids

---
+++ modules/automations/travelform.lua
@@ -0,0 +1,38 @@
+local _, addon = ...
+
+-- automatically swap back to flight form
+
+if addon.PLAYER_CLASS ~= 'DRUID' then
+	return
+end
+
+local trySwapNext
+local function swap()
+	if IsInInstance() then
+		-- don't bother with this if we're in an instance
+		return
+	end
+
+	local shapeshiftID = GetShapeshiftFormID()
+	if trySwapNext then
+		-- if the form is (ground) travel form, try to fly
+		if shapeshiftID == 3 then
+			-- don't attempt when we can't or shouldn't
+			if not InCombatLockdown() and IsFlyableArea() and not IsSubmerged() then
+				-- cancelling the form will exit the forced swap, i.e. turn back into flying
+				CancelShapeshiftForm()
+				trySwapNext = nil
+			end
+		else
+			-- if it's not then give up
+			trySwapNext = nil
+		end
+	elseif shapeshiftID == 3 then
+		-- currently in (ground) travel form, try to swap next time an event triggers
+		trySwapNext = true
+	end
+end
+
+addon:RegisterEvent('MOUNT_JOURNAL_USABILITY_CHANGED', swap)
+addon:RegisterEvent('UPDATE_SHAPESHIFT_FORM', swap)
+addon:RegisterEvent('PLAYER_REGEN_ENABLED', swap)
