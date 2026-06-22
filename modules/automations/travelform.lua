56446e8058a5ad3aef8fa80c7d75299376bf7a96
Adrian L Lange <git@p3lim.net>
2026-06-22 18:58:35 +0200

    Fix for neighborhoods and use our enums

--- modules/automations/travelform.lua
+++ modules/automations/travelform.lua
@@ -8,7 +8,8 @@ end
 
 local trySwapNext
 local function swap()
-	if IsInInstance() then
+	local inInstance, instanceType = IsInInstance()
+	if inInstance and instanceType ~= 'neighborhood' then
 		-- don't bother with this if we're in an instance
 		return
 	end
@@ -16,7 +17,7 @@ local function swap()
 	local shapeshiftID = GetShapeshiftFormID()
 	if trySwapNext then
 		-- if the form is (ground) travel form, try to fly
-		if shapeshiftID == 3 then
+		if shapeshiftID == addon.enums.DruidForms.Travel then
 			-- don't attempt when we can't or shouldn't
 			if not InCombatLockdown() and IsFlyableArea() and not IsSubmerged() then
 				-- cancelling the form will exit the forced swap, i.e. turn back into flying
@@ -27,7 +28,7 @@ local function swap()
 			-- if it's not then give up
 			trySwapNext = nil
 		end
-	elseif shapeshiftID == 3 then
+	elseif shapeshiftID == addon.enums.DruidForms.Travel then
 		-- currently in (ground) travel form, try to swap next time an event triggers
 		trySwapNext = true
 	end
