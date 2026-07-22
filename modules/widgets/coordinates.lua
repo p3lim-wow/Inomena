44ae5bdeba32ad5a8378adccf65d737dd354a7fe
Adrian L Lange <git@p3lim.net>
2026-07-22 17:07:58 +0200

    Add zone name as a suffix like blizzard does

--- modules/widgets/coordinates.lua
+++ modules/widgets/coordinates.lua
@@ -2,7 +2,7 @@
 -- replace world map title text with coordinates
 
 local function updateCoordinates(canvas)
-	local x, y
+	local x, y, suffix
 	local r, g, b
 
 	if canvas:IsMouseOver() then
@@ -10,15 +10,26 @@ local function updateCoordinates(canvas)
 		r, g, b = 0, 1, 0
 	else
 		local position = C_Map.GetPlayerMapPosition(WorldMapFrame:GetMapID(), 'player')
+		if not position then
+			local playerMapID = C_Map.GetBestMapForUnit('player')
+			if playerMapID then
+				position = C_Map.GetPlayerMapPosition(playerMapID, 'player')
+
+				local mapInfo = C_Map.GetMapInfo(playerMapID)
+				if mapInfo and mapInfo.name then
+					suffix = ' |cffababab(in ' .. mapInfo.name .. ')|r'
+				end
+			end
+		end
+
 		if position then
 			x, y = position:GetXY()
 		end
-
 		r, g, b = 1, 1, 0
 	end
 
 	if x and y then
-		WorldMapFrameTitleText:SetFormattedText('%.2f, %.2f', x * 100, y * 100)
+		WorldMapFrameTitleText:SetFormattedText('%.2f, %.2f%s', x * 100, y * 100, suffix or '')
 		WorldMapFrameTitleText:SetTextColor(r, g, b)
 	else
 		WorldMapFrameTitleText:SetText(UNAVAILABLE)
