12e6a37f0ca80c85a6a40c9977feb48514b5c54a
Adrian L Lange <git@p3lim.net>
2026-07-25 14:49:40 +0200

    Fix health colors for secrets

--- modules/units/elements/health.lua
+++ modules/units/elements/health.lua
@@ -8,11 +8,17 @@ function addon.unitShared.UpdateColorHealth(self, _, unit)
 	local color
 	if UnitIsPlayer(unit) or UnitTreatAsPlayerForDisplay(unit) then
 		local _, classToken = UnitClass(unit)
-		color = C_ClassColor.GetClassColor(classToken)
-	elseif UnitIsTapDenied(unit) then
-		color = self.colors.tapped
-	else
-		color = self.colors.reaction[UnitReaction(unit, 'player')]
+		if classToken ~= nil then
+			color = C_ClassColor.GetClassColor(classToken)
+		end
+	end
+
+	if color == nil then
+		if UnitIsTapDenied(unit) then
+			color = self.colors.tapped
+		else
+			color = self.colors.reaction[UnitReaction(unit, 'player')]
+		end
 	end
 
 	if color then
