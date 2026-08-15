3bcc43cb41d336afbf4182ff8b53628310c4803b
Adrian L Lange <git@p3lim.net>
2026-08-16 00:41:59 +0200

    Show CC and Mend Pet on pet

--- modules/units/pet.lua
+++ modules/units/pet.lua
@@ -19,6 +19,32 @@ oUF:RegisterStyle(styleName, function(self)
 		Health:SetPointsOffset(0, -10)
 	end
 
+	local Auras = self:CreateAuras({
+		layoutLimit = math.huge,
+		growthX = 'LEFT',
+		growthY = 'DOWN',
+		initialAnchor = 'TOPRIGHT',
+	})
+	Auras:SetPoint('TOPRIGHT', self, 'TOPLEFT', -addon.SPACING, 0)
+	Auras.elementSpacing = addon.SPACING
+	Auras.lineSpacing = addon.SPACING
+	Auras.size = self:GetHeight(1.2)
+	Auras.tooltipAnchor = 'ANCHOR_TOPRIGHT'
+	Auras.tooltipOffsetX = 1
+	Auras.tooltipOffsetY = 3
+	Auras.PostCreateButton = addon.unitShared.PostCreateAura
+	Auras:AddGroup('HARMFUL|CROWD_CONTROL', {
+		size = self:GetHeight() * 3 -- emphasize!
+	})
+	Auras:AddGroup('HELPFUL|PLAYER', {
+		disableCooldownText = true, -- they're too small to see
+		candidateFilters = {
+			includeSpellIDs = {
+				[136] = true, -- Mend Pet
+			}
+		}
+	})
+
 	local Status = self:CreateText()
 	Status:SetPoint('CENTER', Health)
 	self:Tag(Status, '[inomena:dead]')
