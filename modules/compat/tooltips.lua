869b585516b0e57b9c362f04fbeda2dcd497f1a9
Adrian L Lange <git@p3lim.net>
2026-09-05 00:39:28 +0200

    Add note for WQT's tooltip

--- modules/compat/tooltips.lua
+++ modules/compat/tooltips.lua
@@ -6,7 +6,7 @@ for tooltip, addonName in next, {
 	InteractiveWormholesTooltip = 'InteractiveWormholes',
 	NotGameTooltip1 = 'OPie',
 	RematchGameTooltip = 'Rematch',
-	WQT_GameTooltip = 'WorldQuestTab',
+	WQT_GameTooltip = 'WorldQuestTab', -- fails to use shoppingTooltips table correctly
 	WQT_ShoppingTooltip1 = 'WorldQuestTab',
 	WQT_ShoppingTooltip2 = 'WorldQuestTab',
 } do
